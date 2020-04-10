9aea30c8884b19c27d02432b45cf8d369b0f01a5
PIORS <PIORS@WINDOWS-F5IL54M>
2020-04-10 08:45:58 +0700

    hoclenho

--- src/constants/systemVars.js
+++ src/constants/systemVars.js
@@ -13,7 +13,7 @@ export const API_HOST_LIST = {
     PIMAPI: {
         HostName: "PIMAPI",
             //HostBaseURL: 'http://tmsapi.tterpbeta.vn/'
-              HostBaseURL: 'https://localhost:44338/'
+              HostBaseURL: 'http://devtmsapi.tterpbeta.vn/'
     },
     CacheAPI: {
         HostName: "CacheAPI",
