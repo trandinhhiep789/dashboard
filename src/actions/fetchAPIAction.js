89b8a96c518f4d3a556a223ff355a37e53d61bb7
Le Hoang Vu <ThamTuMeoDen13@Les-MacBook-Pro.local>
2020-05-04 11:13:15 +0700

    fix error router

--- src/actions/fetchAPIAction.js
+++ src/actions/fetchAPIAction.js
@@ -1,14 +1,13 @@
-import {FETCH_API_REQUEST, FETCH_API_SUCCESS, FETCH_API_FAILURE}  from "../constants/actionTypes";
-import {AUTHEN_HOSTNAME,  AUTHEN_HOST_BASEURL, API_HOST_LIST,CLIENT_INFO_OBJECT_STORENAME}  from "../constants/systemVars.js";
+import { FETCH_API_REQUEST, FETCH_API_SUCCESS, FETCH_API_FAILURE } from "../constants/actionTypes";
+import { AUTHEN_HOSTNAME, AUTHEN_HOST_BASEURL, API_HOST_LIST, CLIENT_INFO_OBJECT_STORENAME } from "../constants/systemVars.js";
 import WebRequest from "../common/library/net/WebRequest.js";
 import MD5Digest from "../common/library/cryptography/MD5Digest.js";
-import {GUID,  CreateAuthenData,CheckIsRegisterClient} from "../common/library/AuthenLib.js";
-import {callRegisterClient} from "./registerClient";
+import { GUID, CreateAuthenData, CheckIsRegisterClient } from "../common/library/AuthenLib.js";
+import { callRegisterClient } from "./registerClient";
 import indexedDBLib from "../common/library/indexedDBLib.js";
 
-export function fetchAPIRequest(hostname,hostURL,postData)
-{
-  //  console.log(FETCH_API_REQUEST);
+export function fetchAPIRequest(hostname, hostURL, postData) {
+    //  console.log(FETCH_API_REQUEST);
     return {
         type: FETCH_API_REQUEST,
         Hostname: hostname,
@@ -17,9 +16,8 @@ export function fetchAPIRequest(hostname,hostURL,postData)
     };
 }
 
-export function fetchAPISuccess(resultObject, resultMessage)
-{
-  //  console.log(FETCH_API_SUCCESS, resultObject);
+export function fetchAPISuccess(resultObject, resultMessage) {
+    //  console.log(FETCH_API_SUCCESS, resultObject);
     //console.log(FETCH_API_SUCCESS);
     return {
         type: FETCH_API_SUCCESS,
@@ -30,9 +28,8 @@ export function fetchAPISuccess(resultObject, resultMessage)
 }
 
 
-export function fetchAPIFailure(errorStatus, resultMessage)
-{
-   // console.log(FETCH_API_FAILURE,resultMessage);
+export function fetchAPIFailure(errorStatus, resultMessage) {
+    // console.log(FETCH_API_FAILURE,resultMessage);
     return {
         type: FETCH_API_FAILURE,
         ErrorStatus: errorStatus,
@@ -42,68 +39,54 @@ export function fetchAPIFailure(errorStatus, resultMessage)
 
 
 
-export function callFetchAPI(hostname,apiPath,postData)
-{
+export function callFetchAPI(hostname, apiPath, postData) {
     return (dispatch, getState) => {
         const state = getState();
-        if(state.FetchAPIInfo.IsFetchAPIRequest)
-        {
+        if (state.FetchAPIInfo.IsFetchAPIRequest) {
             // return  {
             //     IsError: true,
             //     StatusID: 100,
             //     Message: "Đang gọi API"
             // };
         }
-            
-        if(!CheckIsRegisterClient(state.RegisterClientInfo[hostname]))
-        {
+        if (!CheckIsRegisterClient(state.RegisterClientInfo[hostname])) {
             const username = state.LoginInfo.Username;
-            const password  = "e10adc3949ba59abbe56e057f20f883e";
-            return dispatch(callRegisterClient(hostname,username, password)).then((registerResult) =>
-            {
-                if(!registerResult.IsError)
-                {
-                    
-                    return dispatch(callAPI(hostname,apiPath,postData));
+            const password = "e10adc3949ba59abbe56e057f20f883e";
+            return dispatch(callRegisterClient(hostname, username, password)).then((registerResult) => {
+                if (!registerResult.IsError) {
+
+                    return dispatch(callAPI(hostname, apiPath, postData));
                 }
-                else
-                {
-                    return  {
+                else {
+                    return {
                         IsError: true,
                         StatusID: 100,
                         Message: registerResult.Message
                     };
                 }
             }
-        
-        );
+
+            );
         }
-        else
-        {
-             return dispatch(callAPI(hostname,apiPath,postData)).then((apiResult) =>
-             {
-                if(apiResult.StatusID == 7)
-                {
+        else {
+            return dispatch(callAPI(hostname, apiPath, postData)).then((apiResult) => {
+                if (apiResult.StatusID == 7) {
                     const username = state.LoginInfo.Username;
-                    const password  = state.LoginInfo.Password;
+                    const password = state.LoginInfo.Password;
                     const db = new indexedDBLib(CLIENT_INFO_OBJECT_STORENAME);
-                    db.delete(hostname).catch((error)  =>
-                        {
-                            console.log("Error Delete indexedDBLib key:", error);
-                        }
+                    db.delete(hostname).catch((error) => {
+                        console.log("Error Delete indexedDBLib key:", error);
+                    }
 
                     );
 
-                    return dispatch(callRegisterClient(hostname,username, password)).then((registerResult) =>
-                    {
-                        if(!registerResult.IsError)
-                        {
-                            
-                            return dispatch(callAPI(hostname,apiPath,postData));
+                    return dispatch(callRegisterClient(hostname, username, password)).then((registerResult) => {
+                        if (!registerResult.IsError) {
+
+                            return dispatch(callAPI(hostname, apiPath, postData));
                         }
-                        else
-                        {
-                            return  {
+                        else {
+                            return {
                                 IsError: true,
                                 StatusID: 100,
                                 Message: registerResult.Message
@@ -112,20 +95,18 @@ export function callFetchAPI(hostname,apiPath,postData)
                     });
 
                 }
-                else
-                {
-                    return  apiResult;
+                else {
+                    return apiResult;
                 }
-             });
+            });
         }
     }
-        
+
 }
 
 
 
-export function callAPI(hostname,apiPath,postData)
-{
+export function callAPI(hostname, apiPath, postData) {
     return (dispatch, getState) => {
         const state = getState();
         // if(state.FetchAPIInfo.IsFetchAPIRequest)
@@ -137,60 +118,67 @@ export function callAPI(hostname,apiPath,postData)
         //         Message: "Đang gọi API"
         //     };
         // }
-           
-          
-        
+
+
+
         const clientID = state.RegisterClientInfo[hostname].ClientID;
         const clientPrivateKey = state.RegisterClientInfo[hostname].ClientPrivateKey;
 
         const hostURL = API_HOST_LIST[hostname].HostBaseURL + apiPath;
-        
         //console.log("callfetchAPI:", state); 
-        dispatch(fetchAPIRequest(hostname,hostURL,postData));
-        const authenData = CreateAuthenData(hostname,state);
+        dispatch(fetchAPIRequest(hostname, hostURL, postData));
+        const authenData = CreateAuthenData(hostname, state);
         //console.log("callfetchAPI authenData:", authenData); 
-        const authenHeader = {ClientID: clientID, AuthenData: authenData};
+        const authenHeader = { ClientID: clientID, AuthenData: authenData };
 
-        return WebRequest.postDataWithAuthenHeader(hostURL, postData,authenHeader).then((apiResult) =>
-         {
-            if(apiResult.IsError === false)
-            {
-        
+        return WebRequest.postDataWithAuthenHeader(hostURL, postData, authenHeader).then((apiResult) => {
+            if (apiResult.IsError === false) {
+
                 dispatch(fetchAPISuccess(apiResult.ResultObject, apiResult.Message));
-                return  {
+                return {
                     IsError: false,
                     StatusID: apiResult.StatusID,
                     Message: apiResult.Message,
                     ResultObject: apiResult.ResultObject,
                     MessageDetail: apiResult.MessageDetail
                 };
-                    
+
             }
-            else
-            {
-              //  console.log(apiResult.Message);
+            else if (apiResult.IsError === undefined) {
+                //  console.log(apiResult.Message);
                 dispatch(fetchAPIFailure(apiResult.StatusID, apiResult.Message));
-                
-                //console.log("Call API Error Detail: ", apiResult.MessageDetail);
-                return  {
+
+                //console.log("Call API Error Detail: ", apiResult);
+                return {
+                    IsError: true,
+                    StatusID: apiResult.StatusID,
+                    Message: "Lỗi hệ thống vui lòng liên hệ quản trị viên.",
+                    MessageDetail: apiResult.MessageDetail ? apiResult.MessageDetail : apiResult.ExceptionMessage
+                };
+            }
+            else {
+                //  console.log(apiResult.Message);
+                dispatch(fetchAPIFailure(apiResult.StatusID, apiResult.Message));
+
+                return {
                     IsError: true,
                     StatusID: apiResult.StatusID,
                     Message: apiResult.Message,
                     MessageDetail: apiResult.MessageDetail ? apiResult.MessageDetail : apiResult.ExceptionMessage
                 };
             }
-            
-         }
 
-         ).catch((err) =>{
+        }
+
+        ).catch((err) => {
             console.error(err);
-            dispatch(fetchAPIFailure(1000,err));
-            return  {
+            dispatch(fetchAPIFailure(1000, err));
+            return {
                 IsError: true,
                 StatusID: 1000,
                 Message: "Lỗi gọi API: không kết nối được với máy chủ"
             };
         });
 
-      };
+    };
 }
