2a94d6e9afa7bd0d15a11c939c5b7aa7e04c2874
PIORS <PIORS@WINDOWS-F5IL54M>
2020-06-03 10:48:44 +0700

    hoclenho

--- src/common/constants/AppMenu.js
+++ src/common/constants/AppMenu.js
@@ -10,93 +10,140 @@ const AppMenu = [
     },
     {
         MenuName: 'MD',
-        MenuTitle: 'Khai báo thông tin chung TT',
+        MenuTitle: 'Khai báo',
         LinkTo: '',
         MenuIcon: '',
         SubMenu: [
             {
                 MenuName: 'PartnerType',
-                MenuTitle: 'Danh sách loại đối tác',
-                LinkTo: '/PartnerType',
+                MenuTitle: 'Đối tác',
+                LinkTo: '',
                 MenuIcon: '',
-                SubMenu: []
-            },
-            {
-                MenuName: 'Partner',
-                MenuTitle: 'Danh sách đối tác',
-                LinkTo: '/Partner',
-                MenuIcon: '',
-                SubMenu: []
-            },          
-            {
-                MenuName: 'CancelDeliveryReason',
-                MenuTitle: 'Danh sách lý do hủy giao hàng',
-                LinkTo: '/CancelDeliveryReason',
-                MenuIcon: '',
-                SubMenu: []
+                SubMenu: [
+                    {
+                        MenuName: 'PartnerType',
+                        MenuTitle: 'Danh sách loại đối tác',
+                        LinkTo: '/PartnerType',
+                        MenuIcon: '',
+                        SubMenu: []
+                    },
+                    {
+                        MenuName: 'Partner',
+                        MenuTitle: 'Danh sách đối tác',
+                        LinkTo: '/Partner',
+                        MenuIcon: '',
+                        SubMenu: []
+                    }
+                ]
             },
             {
                 MenuName: 'CarrierType',
-                MenuTitle: 'Danh sách loại phương tiện vận chuyển',
-                LinkTo: '/CarrierType',
+                MenuTitle: 'vận chuyển',
+                LinkTo: '',
                 MenuIcon: '',
-                SubMenu: []
-            },
-            {
-                MenuName: 'PackageType',
-                MenuTitle: 'Danh sách loại đóng gói hàng hóa vận chuyển',
-                LinkTo: '/PackageType',
-                MenuIcon: '',
-                SubMenu: []
-            },
-            {
-                MenuName: 'ShipmentFeePaymentMethod',
-                MenuTitle: 'Danh sách phương thức thanh toán chi phí vận chuyển',
-                LinkTo: '/ShipmentFeePaymentMethod',
-                MenuIcon: '',
-                SubMenu: []
-            } ,
-            {
-                MenuName: 'ShipmentServiceType',
-                MenuTitle: 'Danh sách loại dịch vụ vận chuyển',
-                LinkTo: '/ShipmentServiceType',
-                MenuIcon: '',
-                SubMenu: []
-            },
-            {
-                MenuName: 'ShipmentGoodsType',
-                MenuTitle: 'Danh sách loại hàng hóa vận chuyển',
-                LinkTo: '/ShipmentGoodsType',
-                MenuIcon: '',
-                SubMenu: []
-            } ,
-            {
-                MenuName: 'ShipmentOrderStatus',
-                MenuTitle: 'Danh sách trạng thái yêu cầu vận chuyển',
-                LinkTo: '/ShipmentOrderStatus',
-                MenuIcon: '',
-                SubMenu: []
+                SubMenu: [
+                    {
+                        MenuName: 'CancelDeliveryReason',
+                        MenuTitle: 'Danh sách lý do hủy giao hàng',
+                        LinkTo: '/CancelDeliveryReason',
+                        MenuIcon: '',
+                        SubMenu: []
+                    },
+                    {
+                        MenuName: 'CarrierType',
+                        MenuTitle: 'Danh sách loại phương tiện vận chuyển',
+                        LinkTo: '/CarrierType',
+                        MenuIcon: '',
+                        SubMenu: []
+                    },
+                    {
+                        MenuName: 'PackageType',
+                        MenuTitle: 'Danh sách loại đóng gói hàng hóa vận chuyển',
+                        LinkTo: '/PackageType',
+                        MenuIcon: '',
+                        SubMenu: []
+                    },
+                    {
+                        MenuName: 'ShipmentFeePaymentMethod',
+                        MenuTitle: 'Danh sách phương thức thanh toán chi phí vận chuyển',
+                        LinkTo: '/ShipmentFeePaymentMethod',
+                        MenuIcon: '',
+                        SubMenu: []
+                    } ,
+                    {
+                        MenuName: 'ShipmentServiceType',
+                        MenuTitle: 'Danh sách loại dịch vụ vận chuyển',
+                        LinkTo: '/ShipmentServiceType',
+                        MenuIcon: '',
+                        SubMenu: []
+                    },
+                    {
+                        MenuName: 'ShipmentGoodsType',
+                        MenuTitle: 'Danh sách loại hàng hóa vận chuyển',
+                        LinkTo: '/ShipmentGoodsType',
+                        MenuIcon: '',
+                        SubMenu: []
+                    } ,
+                    {
+                        MenuName: 'ShipmentOrderStatus',
+                        MenuTitle: 'Danh sách trạng thái yêu cầu vận chuyển',
+                        LinkTo: '/ShipmentOrderStatus',
+                        MenuIcon: '',
+                        SubMenu: []
+                    }
+
+                ]
             },
             {
                 MenuName: 'ShipmentOrderType',
-                MenuTitle: 'Loại yêu cầu vận chuyển',
-                LinkTo: '/ShipmentOrderType',
+                MenuTitle: 'Yêu cầu vận chuyển',
+                LinkTo: '',
                 MenuIcon: '',
-                SubMenu: []
+                SubMenu: [
+                    {
+                        MenuName: 'ShipmentOrderType',
+                        MenuTitle: 'Loại yêu cầu vận chuyển',
+                        LinkTo: '/ShipmentOrderType',
+                        MenuIcon: '',
+                        SubMenu: []
+                    },
+                    {
+                        MenuName: 'ShipmentOrderStep',
+                        MenuTitle: 'Danh sách bước xử lý yêu cầu vận chuyển',
+                        LinkTo: '/ShipmentOrderStep',
+                        MenuIcon: '',
+                        SubMenu: []
+                    }
+
+                ]
             },
             {
-                MenuName: 'ShipmentOrderStep',
-                MenuTitle: 'Danh sách bước xử lý yêu cầu vận chuyển',
-                LinkTo: '/ShipmentOrderStep',
+                MenuName: 'INSTALL',
+                MenuTitle: 'Thông tin lắp đặt',
+                LinkTo: '',
                 MenuIcon: '',
-                SubMenu: []
-            }
-            
+                SubMenu: [
+                    {
+                        MenuName: 'InstallBundle',
+                        MenuTitle: 'Danh sách gói sản phẩm lắp đặt kèm theo',
+                        LinkTo: '/InstallBundle',
+                        MenuIcon: '',
+                        SubMenu: []
+                    },
+                    {
+                        MenuName: 'InstallMaterial',
+                        MenuTitle: 'Danh sách nhóm sản phẩm cần vật tư lắp đặt',
+                        LinkTo: '/InstallMaterial',
+                        MenuIcon: '',
+                        SubMenu: []
+                    }
+                ]
+            },
         ]
     },
     {
         MenuName: 'PartnerPriviledge',
-        MenuTitle: 'Khai báo thông tin nhà cung cấp',
+        MenuTitle: 'Nhà cung cấp',
         LinkTo: '',
         MenuIcon: '',
         SubMenu: [
@@ -131,6 +178,21 @@ const AppMenu = [
         ]
     },
     {
+        MenuName: 'HD',
+        MenuTitle: 'Hợp đồng',
+        LinkTo: '',
+        MenuIcon: '',
+        SubMenu: [
+            {
+                MenuName: 'dđ',
+                MenuTitle: 'Danh sách hợp đồng',
+                LinkTo: '/',
+                MenuIcon: '',
+                SubMenu: []
+            }
+        ]
+    },
+    {
         MenuName: 'ShipmentOrder',
         MenuTitle: 'Vận chuyển',
         LinkTo: '',
@@ -144,29 +206,8 @@ const AppMenu = [
                 SubMenu: []
             }
         ]
-    },
-    {
-        MenuName: 'INSTALL',
-        MenuTitle: 'Khai báo thông tin lắp đặt',
-        LinkTo: '',
-        MenuIcon: '',
-        SubMenu: [
-            {
-                MenuName: 'InstallBundle',
-                MenuTitle: 'Danh sách gói sản phẩm lắp đặt kèm theo',
-                LinkTo: '/InstallBundle',
-                MenuIcon: '',
-                SubMenu: []
-            },
-            {
-                MenuName: 'InstallMaterial',
-                MenuTitle: 'Danh sách nhóm sản phẩm cần vật tư lắp đặt',
-                LinkTo: '/InstallMaterial',
-                MenuIcon: '',
-                SubMenu: []
-            }
-        ]
-    },
+    }
+   
 ];
 
 export default AppMenu;
