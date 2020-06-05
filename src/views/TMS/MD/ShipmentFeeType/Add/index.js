4d58048764dbe88ce21b4b535db8e6bd82290a4b
sang <dsadsd@gmail.com>
2020-06-05 13:24:57 +0700

    1

---
+++ src/views/TMS/MD/ShipmentFeeType/Add/index.js
@@ -0,0 +1,129 @@
+import React from "react";
+import { Redirect } from "react-router-dom";
+import { connect } from "react-redux";
+import { Modal, ModalManager, Effect } from "react-dynamic-modal";
+import SimpleForm from "../../../../../common/components/Form/SimpleForm";
+import { MessageModal } from "../../../../../common/components/Modal";
+import {
+    APIHostName,
+    AddAPIPath,
+    AddElementList,
+    MLObjectDefinition,
+    BackLink,
+    AddPagePath,
+    AddLogAPIPath
+} from "../constants";
+import { callFetchAPI } from "../../../../../actions/fetchAPIAction";
+import { updatePagePath } from "../../../../../actions/pageAction";
+import indexedDBLib from "../../../../../common/library/indexedDBLib.js";
+import { CACHE_OBJECT_STORENAME } from "../../../../../constants/systemVars.js";
+import { callGetCache } from "../../../../../actions/cacheAction";
+
+class AddCom extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleCloseMessage = this.handleCloseMessage.bind(this);
+        this.state = {
+            CallAPIMessage: "",
+            IsCallAPIError: false,
+            IsCloseForm: false
+        };
+    }
+
+    componentDidMount() {
+        this.props.updatePagePath(AddPagePath);
+    }
+
+    // handleGetCache() {
+    //     this.props.callGetCache("PIMCACHE.PIMATTRIBUTECATEGORYTYPE").then((result) => {
+    //         console.log("handleGetCache: ", result);
+    //     });
+    // }
+
+    handleSubmitInsertLog(MLObject) {
+        MLObject.ActivityTitle = `Thêm mới danh sách lý do hủy giao hàng: ${MLObject.CancelDeliveryReasonName}`;
+        MLObject.ActivityDetail = `Thêm mới danh sách lý do hủy giao hàng: ${MLObject.CancelDeliveryReasonName} ${"\n"}Mô tả: ${MLObject.Description}`;
+        MLObject.ObjectID = "MD_CANCELDELIVERYREASON";
+        MLObject.ActivityUser = MLObject.CreatedUser;
+        this.props.callFetchAPI(APIHostName, AddLogAPIPath, MLObject);
+    }
+
+
+    handleSubmit(formData, MLObject) {
+        MLObject.CreatedUser = this.props.AppInfo.LoginInfo.Username;
+        MLObject.LoginLogID = JSON.parse(this.props.AppInfo.LoginInfo.TokenString).AuthenLogID;
+        console.log("Data submit",MLObject );
+
+        this.props.callFetchAPI(APIHostName, AddAPIPath, MLObject).then(apiResult => {
+            this.setState({ IsCallAPIError: apiResult.IsError });
+            if(!apiResult.IsError){
+                //this.handleClearLocalCache();
+                //this.handleSubmitInsertLog(MLObject);
+            }            
+            this.showMessage(apiResult.Message);
+        });
+    }
+
+    handleCloseMessage() {
+        if (!this.state.IsCallAPIError) this.setState({ IsCloseForm: true });
+    }
+
+    showMessage(message) {
+        ModalManager.open(
+            <MessageModal
+                title="Thông báo"
+                message={message}
+                onRequestClose={() => true}
+                onCloseModal={this.handleCloseMessage}
+            />
+        );
+    }
+
+    render() {
+        const dataSource = {
+            IsActived: true
+        };
+        if (this.state.IsCloseForm) {
+            return <Redirect to={BackLink} />;
+        }
+        return (
+            <SimpleForm
+                FormName="Thêm loại phí vận chuyển"
+                MLObjectDefinition={MLObjectDefinition} 
+                listelement={AddElementList}
+                onSubmit={this.handleSubmit}
+                FormMessage={this.state.CallAPIMessage}
+                IsErrorMessage={this.state.IsCallAPIError}
+                dataSource={dataSource}
+                BackLink={BackLink}
+                //RequirePermission={ATTRIBUTE_CATEGORY_TYPE_ADD}
+                ref={this.searchref}
+            />
+        );
+    }
+}
+
+const mapStateToProps = state => {
+    return {
+        AppInfo: state,
+        FetchAPIInfo: state.FetchAPIInfo
+    };
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+        updatePagePath: pagePath => {
+            dispatch(updatePagePath(pagePath));
+        },
+        callFetchAPI: (hostname, hostURL, postData) => {
+            return dispatch(callFetchAPI(hostname, hostURL, postData));
+        },
+        callGetCache: (cacheKeyID) => {
+            return dispatch(callGetCache(cacheKeyID));
+        }
+    };
+};
+
+const Add = connect(mapStateToProps, mapDispatchToProps)(AddCom);
+export default Add;
