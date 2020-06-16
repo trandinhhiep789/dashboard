d95020a1d7e92f053cb4bd8438130ce3f0b2ba74
sang <dsadsd@gmail.com>
2020-06-16 10:09:00 +0700

    1

--- src/views/TMS/MD/ShipmentOrder/ShipmentOrderType/Edit/index.js
+++ src/views/TMS/MD/ShipmentOrder/ShipmentOrderType/Edit/index.js
@@ -375,13 +375,12 @@ class EditCom extends React.Component {
     }
 
     resetCombobox() {
-        let listOptionNull = [{ value: -1, label: "------ Chọn ------" }];
         let _ModalFlexShipmentFeeColumnList = this.state.ModalFlexShipmentFeeColumnList_Edit;
         _ModalFlexShipmentFeeColumnList.forEach(function (objElement) {
             if (objElement.Name == "GetFeeType") {
                 objElement.listoption = [{ value: -1, label: "---Vui lòng chọn---" }, { value: 1, label: "Lấy giá trị cố định" }, { value: 2, label: "Lấy từ bảng làm giá" }];
             } else {
-                objElement.listoption = listOptionNull;
+                objElement.listoption = [];
             }
         });
         this.setState({ ModalFlexShipmentFeeColumnList_Edit: _ModalFlexShipmentFeeColumnList });
