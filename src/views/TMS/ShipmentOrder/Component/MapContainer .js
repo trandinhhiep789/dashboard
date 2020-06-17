ea3a8b61acefc78a4fa64b16af43705960f4866d
PIORS <PIORS@WINDOWS-F5IL54M>
2020-06-17 14:25:15 +0700

    hoclenho

--- src/views/TMS/ShipmentOrder/Component/MapContainer .js
+++ src/views/TMS/ShipmentOrder/Component/MapContainer .js
@@ -61,11 +61,6 @@ export class MapContainer extends Component {
     }
 
     componentWillReceiveProps(nextProps) {
-<<<<<<< HEAD
-debugger;
-=======
-        
->>>>>>> 0620da573e3b7e1890280b722f191836ba016134
         if (JSON.stringify(this.props.SenderGeoLocation) !== JSON.stringify(nextProps.SenderGeoLocation)) {
             const values = nextProps.SenderGeoLocation.split(",")
             const v1 = parseFloat(values[0])
