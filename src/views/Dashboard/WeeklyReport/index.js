80b182fd38b4c11f9263616d99292f36712fd657
PIORS <PIORS@WINDOWS-F5IL54M>
2020-07-30 13:48:43 +0700

    hoclenho

--- src/views/Dashboard/WeeklyReport/index.js
+++ src/views/Dashboard/WeeklyReport/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { callFetchAPI } from "../../../actions/fetchAPIAction";
 import { formatDate } from "../../../common/library/CommonLib.js";
+import { HorizontalBar } from 'react-chartjs-2';
 
 
 class WeeklyReportCom extends Component {
@@ -22,6 +23,30 @@ class WeeklyReportCom extends Component {
         if ( this.props.DataSource != null && this.props.DataSource.length > 0) {
             dataSource = this.props.DataSource[0].WeekShipmentOrderCoordList;
         }
+        const label = [];
+        const data1 = [];
+        if (dataSource.length > 0) {
+            dataSource.map((item, i) => {
+                label.push(formatDate(item.DateDay,true))
+                data1.push(item.CoutShip)
+            })
+        }
+
+        const data = {
+            labels: label,
+            datasets: [{
+                label: 'Ngày điều phối nhiều nhất',
+                data: data1,
+                fill: false,
+                borderColor: '#5b9bd5',
+                backgroundColor: '#5b9bd5',
+                pointBorderColor: '#5b9bd5',
+                pointBackgroundColor: '#5b9bd5',
+                pointHoverBackgroundColor: '#5b9bd5',
+                pointHoverBorderColor: '#5b9bd5',
+            }]
+        };
+
         return (
             <div className="col-md-12 col-md-7 col-lg-7">
                 <div className="card shadow-1">
@@ -29,47 +54,12 @@ class WeeklyReportCom extends Component {
                         <h5 className="card-title">biểu đồ điều phối trong tuần</h5>
                     </div>
                     <div className="card-body">
-                        <div className=" table-responsive">
-                            <table className="table table-sm table-striped table-bordered table-hover table-condensed" cellSpacing="0" >
-                                <thead className="thead-light">
-                                    <tr>
-                                        <th className="jsgrid-header-cell" style={{ width: 190, minWidth: 190 }} >Ngày</th>
-                                        <th className="jsgrid-header-cell" style={{ width: 300, minWidth: 350 }}>Số lượng điều phối</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    {dataSource != null &&
-                                        dataSource.map((rowItem, rowIndex) => {
-                                            let rowClass = "jsgrid-row";
-                                            if (index % 2 != 0) {
-                                                rowClass = "jsgrid-alt-row";
-                                            }
-                                            return (<tr key={rowIndex}>
-                                                <td>{formatDate(rowItem.DateDay)}</td>
-                                                <td>{rowItem.CoutShip}</td>
-                                            </tr>
-                                            );
-                                        })
-                                    }
-                                </tbody>
-                            </table>
-                        </div>
+                    <HorizontalBar data={data} height={197} />
                     </div>
                 </div>
             </div>
         );
-        return (
-            <div className="col-md-12 col-md-7 col-lg-7">
-                <div className="card shadow-1">
-                    <div className="card-header">
-                        <h5 className="card-title">biểu đồ điều phối trong tuần</h5>
-                    </div>
-                    <div className="card-body">
-                        aa
-                    </div>
-                </div>
-            </div>
-        );
+       
     }
 }
 
