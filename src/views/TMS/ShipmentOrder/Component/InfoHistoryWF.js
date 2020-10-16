0c24612125b75758a439a71e35fc825ba0aeb3e3
Phận Nguyễn Văn <phan.nguyenvan>
2020-10-16 15:31:43 +0700

    change width maps

--- src/views/TMS/ShipmentOrder/Component/InfoHistoryWF.js
+++ src/views/TMS/ShipmentOrder/Component/InfoHistoryWF.js
@@ -11,7 +11,7 @@ import ReactTooltip from 'react-tooltip';
 const containerStyle = {
     position: 'absolute',
     width: '98%',
-    height: '500px'
+    height: '480px'
 }
 class InfoHistoryWFCom extends Component {
     constructor(props) {
