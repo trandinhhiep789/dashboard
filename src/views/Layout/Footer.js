e2ddae30ed7abc7e306b5979ef19dcfb74f99927
PIORS <PIORS@WINDOWS-F5IL54M>
2020-12-21 13:50:58 +0700

    hoclenho

--- src/views/Layout/Footer.js
+++ src/views/Layout/Footer.js
@@ -1,24 +1,23 @@
-import React from "react";
-import ReactDOM from "react-dom";
-export default function Footer()
-{
-    return(
-        <footer className="site-footer">
-        <div className="container">
-          <div className="row">
-            <div className="col-md-6">
-              <p className="text-center text-md-left">Copyright © 2018 The Gioi Di Dong</p>
-            </div>
-
-            <div className="col-md-6">
-              <ul className="nav nav-primary nav-dotted nav-dot-separated justify-content-center justify-content-md-end">
-			  <p className="text-center text-md-left">ERP</p>
-               
-              
-              </ul>
+import React, { Component } from 'react'
+export class Footer extends Component {
+    render() {
+        return (
+          <footer className="site-footer">
+          <div className="container">
+            <div className="row">
+              <div className="col-md-6">
+                <p className="text-center text-md-left">Copyright © 2018 The Gioi Di Dong</p>
+              </div>
+  
+              <div className="col-md-6">
+                <ul className="nav nav-primary nav-dotted nav-dot-separated justify-content-center justify-content-md-end">
+                <p className="text-center text-md-left">ERP</p>
+                </ul>
+              </div>
             </div>
           </div>
-        </div>
-      </footer>
- );
+        </footer>
+        )
+    }
 }
+export default Footer;
