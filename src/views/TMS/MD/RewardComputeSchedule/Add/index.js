8fcbc63d933bc6d3e1a2e5f5b6ca975d18841dd0
Mr. Sang <thucvuvan.it@gmail.com>
2021-01-04 09:35:43 +0700

    1

--- src/views/TMS/MD/RewardComputeSchedule/Add/index.js
+++ src/views/TMS/MD/RewardComputeSchedule/Add/index.js
@@ -16,7 +16,8 @@ import { callFetchAPI } from "../../../../../actions/fetchAPIAction";
 import { updatePagePath } from "../../../../../actions/pageAction";
 import { REWARDCOMPUTESCHEDULE_ADD } from "../../../../../constants/functionLists";
 import { callGetCache, callClearLocalCache } from "../../../../../actions/cacheAction";
-import { ERPCOMMONCACHE_AREATYPE, ERPCOMMONCACHE_MATERIALGROUP } from "../../../../../constants/keyCache";
+import ReactNotification from "react-notifications-component";
+import "react-notifications-component/dist/theme.css";
 
 
 class AddCom extends React.Component {
@@ -27,34 +28,129 @@ class AddCom extends React.Component {
         this.state = {
             CallAPIMessage: "",
             IsCallAPIError: false,
-            IsCloseForm: false
+            IsCloseForm: false,
+            cssNotification: "notification-danger",
+            iconNotification: "fa fa-exclamation"
         };
+        this.notificationDOMRef = React.createRef();
+    }
+
+    addNotification(message1, IsError) {
+        if (!IsError) {
+            this.setState({
+                cssNotification: "notification-custom-success",
+                iconNotification: "fa fa-check"
+            });
+        } else {
+            this.setState({
+                cssNotification: "notification-danger",
+                iconNotification: "fa fa-exclamation"
+            });
+        }
+        this.notificationDOMRef.current.addNotification({
+            container: "bottom-right",
+            content: (
+                <div className={this.state.cssNotification}>
+                    <div className="notification-custom-icon">
+                        <i className={this.state.iconNotification} />
+                    </div>
+                    <div className="notification-custom-content">
+                        <div className="notification-close">
+                            <span>×</span>
+                        </div>
+                        <h4 className="notification-title">Thông Báo</h4>
+                        <p className="notification-message">{message1}</p>
+                    </div>
+                </div>
+            ),
+            dismiss: { duration: 6000 },
+            dismissable: { click: true }
+        });
     }
 
     componentDidMount() {
         this.props.updatePagePath(AddPagePath);
     }
 
-
     handleSubmit(formData, MLObject) {
         MLObject.CreatedUser = this.props.AppInfo.LoginInfo.Username;
         MLObject.LoginLogID = JSON.parse(this.props.AppInfo.LoginInfo.TokenString).AuthenLogID;
 
-        if (MLObject.RewardDateFrom.getMonth) {
-            MLObject.RewardDateFrom.setDate(MLObject.RewardDateFrom.getDate() + 1);
+
+        var dates = {
+            convert: function (d) {
+                // Converts the date in d to a date-object. The input can be:
+                //   a date object: returned without modification
+                //  an array      : Interpreted as [year,month,day]. NOTE: month is 0-11.
+                //   a number     : Interpreted as number of milliseconds
+                //                  since 1 Jan 1970 (a timestamp) 
+                //   a string     : Any format supported by the javascript engine, like
+                //                  "YYYY/MM/DD", "MM/DD/YYYY", "Jan 31 2009" etc.
+                //  an object     : Interpreted as an object with year, month and date
+                //                  attributes.  **NOTE** month is 0-11.
+                return (
+                    d.constructor === Date ? d :
+                        d.constructor === Array ? new Date(d[0], d[1], d[2]) :
+                            d.constructor === Number ? new Date(d) :
+                                d.constructor === String ? new Date(d) :
+                                    typeof d === "object" ? new Date(d.year, d.month, d.date) :
+                                        NaN
+                );
+            },
+            compare: function (a, b) {
+                // Compare two dates (could be of any type supported by the convert
+                // function above) and returns:
+                //  -1 : if a < b
+                //   0 : if a = b
+                //   1 : if a > b
+                // NaN : if a or b is an illegal date
+                // NOTE: The code inside isFinite does an assignment (=).
+                return (
+                    isFinite(a = this.convert(a).valueOf()) &&
+                        isFinite(b = this.convert(b).valueOf()) ?
+                        (a > b) - (a < b) :
+                        NaN
+                );
+            },
+            inRange: function (d, start, end) {
+                // Checks if date in d is between dates in start and end.
+                // Returns a boolean or NaN:
+                //    true  : if d is between start and end (inclusive)
+                //    false : if d is before start or after end
+                //    NaN   : if one or more of the dates is illegal.
+                // NOTE: The code inside isFinite does an assignment (=).
+                return (
+                    isFinite(d = this.convert(d).valueOf()) &&
+                        isFinite(start = this.convert(start).valueOf()) &&
+                        isFinite(end = this.convert(end).valueOf()) ?
+                        start <= d && d <= end :
+                        NaN
+                );
+            }
         }
 
-        if (MLObject.RewardDateTo.getMonth) {
-            MLObject.RewardDateTo.setDate(MLObject.RewardDateTo.getDate() + 1);
+        let validDate = dates.compare(MLObject.RewardDateFrom, MLObject.RewardDateTo);
+        if (validDate == 1) {
+            this.addNotification("Ngày tính thưởng không hợp lệ. Vui lòng kiểm tra lại.", true);
+        } else {
+
+            if (MLObject.RewardDateFrom.getMonth) {
+                MLObject.RewardDateFrom.setDate(MLObject.RewardDateFrom.getDate() + 1);
+            }
+
+            if (MLObject.RewardDateTo.getMonth) {
+                MLObject.RewardDateTo.setDate(MLObject.RewardDateTo.getDate() + 1);
+            }
+
+            this.props.callFetchAPI(APIHostName, AddAPIPath, MLObject).then(apiResult => {
+                this.setState({ IsCallAPIError: apiResult.IsError });
+                if (!apiResult.IsError) {
+                    //this.props.callClearLocalCache(ERPCOMMONCACHE_MATERIALGROUP);
+                }
+                this.showMessage(apiResult.Message);
+            });
         }
 
-        this.props.callFetchAPI(APIHostName, AddAPIPath, MLObject).then(apiResult => {
-            this.setState({ IsCallAPIError: apiResult.IsError });
-            if(!apiResult.IsError){
-                //this.props.callClearLocalCache(ERPCOMMONCACHE_MATERIALGROUP);
-            }            
-            this.showMessage(apiResult.Message);
-        });
     }
 
     handleCloseMessage() {
@@ -80,18 +176,22 @@ class AddCom extends React.Component {
             return <Redirect to={BackLink} />;
         }
         return (
-            <SimpleForm
-                FormName="Thêm lịch tính thưởng"
-                MLObjectDefinition={MLObjectDefinition} 
-                listelement={AddElementList}
-                onSubmit={this.handleSubmit}
-                FormMessage={this.state.CallAPIMessage}
-                IsErrorMessage={this.state.IsCallAPIError}
-                dataSource={dataSource}
-                BackLink={BackLink}
-                RequirePermission={REWARDCOMPUTESCHEDULE_ADD}
-                ref={this.searchref}
-            />
+            <React.Fragment>
+                <ReactNotification ref={this.notificationDOMRef} />
+                <SimpleForm
+                    FormName="Thêm lịch tính thưởng"
+                    MLObjectDefinition={MLObjectDefinition}
+                    listelement={AddElementList}
+                    onSubmit={this.handleSubmit}
+                    FormMessage={this.state.CallAPIMessage}
+                    IsErrorMessage={this.state.IsCallAPIError}
+                    dataSource={dataSource}
+                    BackLink={BackLink}
+                    RequirePermission={REWARDCOMPUTESCHEDULE_ADD}
+                    ref={this.searchref}
+                />
+            </React.Fragment>
+
         );
     }
 }
