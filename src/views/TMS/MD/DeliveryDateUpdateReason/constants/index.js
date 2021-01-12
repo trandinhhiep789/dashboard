98d04d03825333ade79b4d18ec8e25f9a9b870b9
Mr. Sang <thucvuvan.it@gmail.com>
2021-01-12 10:03:40 +0700

    1

---
+++ src/views/TMS/MD/DeliveryDateUpdateReason/constants/index.js
@@ -0,0 +1,333 @@
+export const APIHostName = "TMSAPI";
+export const SearchAPIPath = "api/DeliveryDateUpdateReason/Search";
+export const LoadAPIPath = "api/DeliveryDateUpdateReason/Load";
+export const AddAPIPath = "api/DeliveryDateUpdateReason/Add";
+export const UpdateAPIPath = "api/DeliveryDateUpdateReason/Update";
+export const DeleteAPIPath = "api/DeliveryDateUpdateReason/Delete";
+export const UpdateOrderAPIPath = "api/DeliveryDateUpdateReason/UpdateOrder";
+export const BackLink = "/DeliveryDateUpdateReason";
+export const AddLink = "/DeliveryDateUpdateReason/Add";
+export const AddLogAPIPath = "api/UserActivity/Add";
+export const IDSelectColumnName = "chkSelect";
+export const PKColumnName = "DeliveryDateUpdateReasonID";
+export const InitSearchParams = [{ SearchKey: "@Keyword", SearchValue: "" }];
+
+export const PagePath = [
+    { Link: "/", Title: "Trang chủ", icon: "fa fa-home" },
+    { Link: "", Title: "Danh sách lý do cập nhật giờ giao hàng" }
+];
+
+export const EditPagePath = [
+    { Link: "/", Title: "Trang chủ", icon: "fa fa-home" },
+    { Link: "/DeliveryDateUpdateReason", Title: "Danh sách lý do cập nhật giờ giao hàng" },
+    { Link: "", Title: "Sửa" }
+];
+
+export const AddPagePath = [
+    { Link: "/", Title: "Trang chủ", icon: "fa fa-home" },
+    { Link: "/DeliveryDateUpdateReason", Title: "Danh sách lý do cập nhật giờ giao hàng" },
+    { Link: "", Title: "Thêm" }
+];
+
+export const SearchElementList = [
+    {
+        type: "text",
+        name: "txtKeyword",
+        label: "Từ khóa:",
+        value: "",
+        placeholder: "",
+        icon: "",
+        listoption: {}
+    }
+];
+
+export const AddElementList = [
+    {
+        type: "text",
+        name: "txtDeliveryDateUpdateReasonID",
+        label: "mã lý do cập nhật giờ GH",
+        value: "",
+        maxSize: "5",
+        placeholder: "",
+        icon: "",
+        listoption: {},
+        DataSourceMember: "DeliveryDateUpdateReasonID",
+        readonly: false,
+        validatonList: ["required", "number"]
+    },
+    {
+        type: "text",
+        name: "txtDeliveryDateUpdateReasonName",
+        label: "tên lý do cập nhật giờ GH",
+        value: "",
+        maxSize: "200",
+        placeholder: "",
+        icon: "",
+        listoption: {},
+        DataSourceMember: "DeliveryDateUpdateReasonName",
+        readonly: false,
+        validatonList: ["required"],
+    },
+    {
+        type: "textarea",
+        name: "txtDescription",
+        label: "Mô tả:",
+        value: "",
+        maxSize: "2000",
+        placeholder: "",
+        icon: "",
+        rows: "6",
+        listoption: {},
+        DataSourceMember: "Description",
+        readonly: false,
+        validatonList: []
+    },
+    {
+        type: "text",
+        name: "txtOrderIndex",
+        label: "Thứ tự hiển thị:",
+        value: "",
+        maxSize: "9",
+        placeholder: "",
+        icon: "",
+        listoption: {},
+        DataSourceMember: "OrderIndex",
+        readonly: false,
+        validatonList: ["number"]
+    },
+    {
+        type: "checkbox",
+        name: "chkIsActived",
+        label: "Kích hoạt:",
+        value: true,
+        placeholder: "",
+        icon: "",
+        listoption: {},
+        readonly: false,
+        validatonList: []
+    },
+    {
+        type: "checkbox",
+        name: "chkIsSystem",
+        label: "Hệ thống:",
+        value: false,
+        placeholder: "",
+        icon: "",
+        listoption: {},
+        readonly: false,
+        validatonList: []
+    }
+];
+
+export const EditElementList = [
+    {
+        type: "text",
+        name: "txtDeliveryDateUpdateReasonID",
+        label: "mã lý do cập nhật giờ GH",
+        value: "",
+        maxSize: "5",
+        placeholder: "",
+        icon: "",
+        listoption: {},
+        DataSourceMember: "DeliveryDateUpdateReasonID",
+        readonly: true,
+        validatonList: ["required", "number"]
+    },
+    {
+        type: "text",
+        name: "txtDeliveryDateUpdateReasonName",
+        label: "tên lý do cập nhật giờ GH",
+        value: "",
+        maxSize: "200",
+        placeholder: "",
+        icon: "",
+        listoption: {},
+        DataSourceMember: "DeliveryDateUpdateReasonName",
+        readonly: false,
+        validatonList: ["required"],
+    },
+    {
+        type: "textarea",
+        name: "txtDescription",
+        label: "Mô tả:",
+        value: "",
+        maxSize: "2000",
+        placeholder: "",
+        icon: "",
+        rows: "6",
+        listoption: {},
+        DataSourceMember: "Description",
+        readonly: false,
+        validatonList: []
+    },
+    {
+        type: "text",
+        name: "txtOrderIndex",
+        label: "Thứ tự hiển thị:",
+        value: "",
+        maxSize: "9",
+        placeholder: "",
+        icon: "",
+        listoption: {},
+        DataSourceMember: "OrderIndex",
+        readonly: false,
+        validatonList: ["number"]
+    },
+    {
+        type: "checkbox",
+        name: "chkIsActived",
+        label: "Kích hoạt:",
+        value: "",
+        placeholder: "",
+        icon: "",
+        listoption: {},
+        DataSourceMember: "IsActived",
+        readonly: false,
+        validatonList: []
+    },
+    {
+        type: "checkbox",
+        name: "chkIsSystem",
+        label: "Hệ thống:",
+        value: "",
+        placeholder: "",
+        icon: "",
+        listoption: {},
+        DataSourceMember: "IsSystem",
+        readonly: false,
+        validatonList: []
+    }
+];
+
+export const SearchMLObjectDefinition = [
+    {
+        Name: "Keyword",
+        DefaultValue: "",
+        BindControlName: "txtKeyword"
+    }
+];
+
+export const MLObjectDefinition = [
+    {
+        Name: "DeliveryDateUpdateReasonID",
+        DefaultValue: "",
+        BindControlName: "txtDeliveryDateUpdateReasonID",
+        DataSourceMember: "DeliveryDateUpdateReasonID"
+    },
+    {
+        Name: "DeliveryDateUpdateReasonName",
+        DefaultValue: "",
+        BindControlName: "txtDeliveryDateUpdateReasonName",
+        DataSourceMember: "DeliveryDateUpdateReasonName"
+    },
+    {
+        Name: "Description",
+        DefaultValue: "",
+        BindControlName: "txtDescription",
+        DataSourceMember: "Description"
+    },
+    {
+        Name: "OrderIndex",
+        DefaultValue: "",
+        BindControlName: "txtOrderIndex",
+        DataSourceMember: "OrderIndex"
+    },
+    {
+        Name: "IsActived",
+        DefaultValue: true,
+        BindControlName: "chkIsActived",
+        DataSourceMember: "IsActived"
+    },
+    {
+        Name: "IsSystem",
+        DefaultValue: false,
+        BindControlName: "chkIsSystem",
+        DataSourceMember: "IsSystem"
+    },
+    {
+        Name: "CreatedUser",
+        DefaultValue: "administrator",
+        BindControlName: "",
+        DataSourceMember: "CreatedUser"
+    },
+    {
+        Name: "UpdatedUser",
+        DefaultValue: "administrator",
+        BindControlName: "",
+        DataSourceMember: "UpdatedUser"
+    },
+    {
+        Name: "LoginLogID",
+        DefaultValue: "",
+        BindControlName: "",
+        DataSourceMember: ""
+    }
+];
+
+export const DataGridColumnList = [
+    {
+        Name: "chkSelect",
+        Type: "checkbox",
+        Caption: "Chọn",
+        DataSourceMember: "DeliveryDateUpdateReasonID",
+        Width: 60
+    },
+    {
+        Name: "DeliveryDateUpdateReasonID",
+        Type: "text",
+        Caption: "Mã lý do cập nhật giờ giao hàng",
+        DataSourceMember: "DeliveryDateUpdateReasonID",
+        Width: 160
+    },
+    {
+        Name: "DeliveryDateUpdateReasonName",
+        Type: "text",
+        Caption: "Tên lý do cập nhật giờ giao hàng",
+        DataSourceMember: "DeliveryDateUpdateReasonName",
+        Width: 250
+    },
+    // {
+    //     Name: "Description",
+    //     Type: "text",
+    //     Caption: "Mô tả",
+    //     DataSourceMember: "Description",
+    //     //Width: 200
+    // },
+    {
+        Name: "IsActived",
+        Type: "checkicon",
+        Caption: "Kích hoạt",
+        DataSourceMember: "IsActived",
+        Width: 80
+    },
+    // {
+    //     Name: "IsSystem",
+    //     Type: "checkicon",
+    //     Caption: "Hệ thống",
+    //     DataSourceMember: "IsSystem",
+    //     Width: 200
+    // },
+    {
+        Name: "CreatedDate",
+        Type: "date",
+        Caption: "Ngày tạo",
+        DataSourceMember: "CreatedDate",
+        Width: 140
+    },
+    {
+        Name: "CreatedUserFullName",
+        Type: "text",
+        Caption: "Người tạo",
+        DataSourceMember: "CreatedUserFullName",
+        Width: 140
+    },
+    {
+        Name: "Action",
+        Type: "link",
+        Caption: "Tác vụ",
+        DataSourceMember: "DeliveryDateUpdateReasonID",
+        Width: 80,
+        Link: "/DeliveryDateUpdateReason/Edit/",
+        LinkText: "Chỉnh sửa"
+    }
+];
