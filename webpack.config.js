3b262d48c6c81d22d5c2331c74c496abbfc2f377
74260 <74260@Q9RDPMSO10-62>
2021-01-27 10:56:25 +0700

    fix chia file bundle

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
-var HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
     entry: ['./src/app.js'],
@@ -14,11 +15,37 @@ module.exports = {
         port: 8089,
         https: false
     },
-    // plugins: [
-    //     new HtmlWebpackPlugin({
-    //         template: 'app/index.html'
-    //     })
-    // ],
+    optimization: {
+        // removeAvailableModules: true,
+        runtimeChunk: 'single',
+        splitChunks: {
+            chunks: 'all',
+            maxInitialRequests: 4,
+            minSize: 0,
+            maxSize: 550000
+        },
+        // minimizer: [
+        //     // new OptimizeCSSAssetsPlugin({}),
+        //     new TerserPlugin({
+        //         extractComments: true,
+        //         cache: true,
+        //         parallel: true,
+        //         sourceMap: true, // Must be set to true if using source-maps in production
+        //         terserOptions: {
+        //             // https://github.com/webpack-contrib/terser-webpack-plugin#terseroptions
+        //             extractComments: 'all',
+        //             compress: {
+        //                 drop_console: true,
+        //             },
+        //         }
+        //     })
+        // ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './index.html'
+        })
+    ],
     // 	devServer: {
     //     historyApiFallback: true,
     //     contentBase: './',
@@ -33,7 +60,7 @@ module.exports = {
                     loader: "babel-loader",
                     options: {
                         presets: ['babel-preset-env', 'babel-preset-react', 'babel-preset-stage-3'],
-                        plugins: [["transform-class-properties","syntax-dynamic-import", { spec: true }]]
+                        plugins: [["transform-class-properties", "syntax-dynamic-import", { spec: true }]]
                     }
                 }
             },
