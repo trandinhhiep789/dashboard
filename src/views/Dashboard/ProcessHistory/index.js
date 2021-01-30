b0a693fafc894fc82e26f8a3cc6c43146b1dc841
74260 <74260@Q9RDPMSO10-62>
2021-01-30 20:25:45 +0700

    update table

--- src/views/Dashboard/ProcessHistory/index.js
+++ src/views/Dashboard/ProcessHistory/index.js
@@ -21,37 +21,44 @@ class ProcessHistoryCom extends Component {
         const dataSource = this.props.DataSource;
         return (
             <div className="col-12 col-md-7 col-lg-7">
-                <div className="card shadow-1">
+                <div className="card shadow-1 Process-history">
                     <div className="card-header">
                         <h5 className="card-title">Nhật ký xử lý</h5>
                     </div>
                     <div className="card-body">
-                        <div className="table-responsive table-custom">
-                            <table className="table table-sm table-striped table-bordered table-hover table-condensed" cellSpacing="0" >
-                                <thead className="thead-light">
-                                    <tr>
-                                        <th className="jsgrid-header-cell" style={{ width: 150 }} >Mã vận đơn</th>
-                                        <th className="jsgrid-header-cell" style={{ width: 150 }}>Thời gian xử lý</th>
-                                        <th className="jsgrid-header-cell" style={{ width: 700 }} >Nội dung</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    {dataSource != null &&
-                                        dataSource.map((rowItem, rowIndex) => {
-                                            let rowClass = "jsgrid-row";
-                                            if (index % 2 != 0) {
-                                                rowClass = "jsgrid-alt-row";
+                        <div className="jsgrid">
+                            <div className="jsgrid-grid-header jsgrid-header-scrollbar">
+                                <table className="jsgrid-table">
+                                    <thead className="jsgrid-header-row">
+                                        <tr>
+                                            <th className="jsgrid-header-cell" style={{ width: '20%' }} >Mã vận đơn</th>
+                                            <th className="jsgrid-header-cell" style={{ width: '20%' }}>Thời gian xử lý</th>
+                                            <th className="jsgrid-header-cell" style={{ width: '60%' }} >Nội dung</th>
+                                        </tr>
+                                    </thead>
+                                </table>
+                                
+                            </div>
+                            <div className="jsgrid-grid-body">
+                                    <table className="jsgrid-table">
+                                        <tbody>
+                                            {dataSource != null &&
+                                                dataSource.map((rowItem, rowIndex) => {
+                                                    let rowClass = "jsgrid-row";
+                                                    if (index % 2 != 0) {
+                                                        rowClass = "jsgrid-alt-row";
+                                                    }
+                                                    return (<tr key={rowIndex} className="jsgrid-row">
+                                                        <td className="jsgrid-cell" style={{ width: '20%' }}>{rowItem.ShipmentOrderID}</td>
+                                                        <td className="jsgrid-cell" style={{ width: '20%' }}>{formatDate(rowItem.CreatedDate)}</td>
+                                                        <td className="jsgrid-cell" style={{ width: '60%' }}>{rowItem.ActionContent}</td>
+                                                    </tr>
+                                                    );
+                                                })
                                             }
-                                            return (<tr key={rowIndex}>
-                                                <td>{rowItem.ShipmentOrderID}</td>
-                                                <td>{formatDate(rowItem.CreatedDate)}</td>
-                                                <td>{rowItem.ActionContent}</td>
-                                            </tr>
-                                            );
-                                        })
-                                    }
-                                </tbody>
-                            </table>
+                                        </tbody>
+                                    </table>
+                                </div>
                         </div>
                     </div>
                 </div>
