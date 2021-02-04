f1f6ee60ae555bad0d6041457cecb03185efe790
142188 <phungtheco1@gmail.com>
2021-02-04 17:36:03 +0700

    style accountinfo page

--- src/views/Account/constants/index.js
+++ src/views/Account/constants/index.js
@@ -4,3 +4,35 @@ export const PagePath = [
     { Link: "/", Title: "Trang chủ", icon: "fa fa-home" },
     { Link: "/accountinfo", Title: "Thông tin cá nhân" }
 ];
+
+export const SidebarAccountInfo = [
+    {
+        MenuName: "CommonInfo",
+        MenuTitle: "Thông tin chung",
+        LinkTo: "",
+        MenuIcon: "",
+        SubMenu: [
+            {
+                MenuName: "CommonInfo1",
+                MenuTitle: "Thông tin chung 1",
+                LinkTo: "/accountinfo",
+                MenuIcon: "",
+                SubMenu: []
+            },
+        ]
+    },
+    {
+        MenuName: "ChangePassword",
+        MenuTitle: "Đổi mật khẩu",
+        LinkTo: "/accountinfo/changepassword",
+        MenuIcon: "",
+        SubMenu: []
+    },
+    {
+        MenuName: "Video",
+        MenuTitle: "Video",
+        LinkTo: "/accountinfo/video",
+        MenuIcon: "",
+        SubMenu: []
+    }
+]
