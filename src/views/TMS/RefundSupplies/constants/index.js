b8c767f550a7e2edb76bba50297f34ba1bb6ebc4
74260 <74260@Q9RDPMSO10-62>
2021-03-05 11:25:25 +0700

    update code yêu cầu nhập trả vật tư

--- src/views/TMS/RefundSupplies/constants/index.js
+++ src/views/TMS/RefundSupplies/constants/index.js
@@ -1,16 +1,40 @@
 export const APIHostName = "TMSAPI";
 export const IDSelectColumnName = "chkSelect";
-export const PKColumnName = "DestroyRequestID";
+export const PKColumnName = "MTReturnRequestID";
+export const BackLink = "/RefundSupplies";
+export const AddLink = "/RefundSupplies/Add";
 
+export const AddAPIPath = "api/MTReturnRequest/Add";
 
-export const TitleFormSearch = "Tìm kiếm danh sách nhập trả vật tư";
-export const TitleFormAdd = "Thêm nhập trả vật tư";
+export const LoadAPIByMtreturnRequestTypeIDPath = "api/MTReturnRequest_ReviewList/LoadByMTReturnRequesTypeID";
+export const LoadAPIByRequestTypeIDPath = "api/MTReturnRequestType_Product/GetListByRequestTypeID";
+
+export const TitleFormSearch = "Tìm kiếm danh sách yêu cầu nhập trả vật tư";
+export const TitleFormAdd = "Thêm yêu cầu nhập trả vật tư";
 export const TitleFormEdit = "Cập nhật nhập trả vật tư";
 export const TitleFormDetail = "Thông tin nhập trảy vật tư";
 
 export const PagePath = [
     { Link: "/", Title: "Trang chủ", icon: "fa fa-home" },
-    { Link: "", Title: "Nhập trả vật tư" }
+    { Link: "", Title: " Yêu cầu nhập trả vật tư" }
+];
+
+export const EditPagePath = [
+    { Link: "/", Title: "Trang chủ", icon: "fa fa-home" },
+    { Link: "/RefundSupplies", Title: "Danh sách  yêu cầu nhập trả vật tư" },
+    { Link: "", Title: "Cập nhật" }
+];
+
+export const AddPagePath = [
+    { Link: "/", Title: "Trang chủ", icon: "fa fa-home" },
+    { Link: "/RefundSupplies", Title: "Danh sách  yêu cầu nhập trả vật tư" },
+    { Link: "", Title: "Thêm" }
+];
+
+export const DetailAPIPath = [
+    { Link: "/", Title: "Trang chủ", icon: "fa fa-home" },
+    { Link: "/RefundSupplies", Title: "Danh sách yêu cầu nhập trả vật tư" },
+    { Link: "", Title: "Chi tiết" }
 ];
 
 export const SearchMLObjectDefinition = [
@@ -102,4 +126,139 @@ export const DataGridColumnList = [
         Link: "/DestroyRequest/Edit/",
         LinkText: "Chỉnh sửa"
     },
+];
+
+export const MLObjectDefinition = [
+    {
+        Name: "MtreturnRequestTypeID",
+        DefaultValue: "",
+        BindControlName: "cboMtreturnRequestType",
+        DataSourceMember: "MtreturnRequestTypeID"
+    },
+    {
+        Name: "MtreturnRequestTypeID",
+        DefaultValue: "",
+        BindControlName: "cboMtreturnRequestType",
+        DataSourceMember: "MtreturnRequestTypeID"
+    },
+    {
+        Name: "MTReturnRequestTitle",
+        DefaultValue: "",
+        BindControlName: "txtMTReturnRequestTitle",
+        DataSourceMember: "MTReturnRequestTitle"
+    },
+    {
+        Name: "RequestStoreID",
+        DefaultValue: "",
+        BindControlName: "cboRequestStore",
+        DataSourceMember: "RequestStoreID"
+    },
+    {
+        Name: "RequestDate",
+        DefaultValue: "",
+        BindControlName: "dtRequestDate",
+        DataSourceMember: "RequestDate"
+    },
+    {
+        Name: "Description",
+        DefaultValue: "",
+        BindControlName: "txtDescription",
+        DataSourceMember: "Description"
+    },
+    {
+        Name: "lstMTReturnRequestDetail",
+        DefaultValue: {},
+        BindControlName: "lstMTReturnRequestDetail",
+        DataSourceMember: "lstMTReturnRequestDetail"
+    },
+    {
+        Name: "lstMTReturnRequestReviewLevel",
+        DefaultValue: {},
+        BindControlName: "lstMTReturnRequestReviewLevel",
+        DataSourceMember: "lstMTReturnRequestReviewLevel"
+    }
+];
+
+export const InputMTReturnRequestDetailColumnList = [
+    {
+        Name: "MaterialGroupID",
+        Type: "text",
+        Caption: "Nhóm vật tư",
+        DataSourceMember: "MaterialGroupID",
+        Width: 100
+    },
+    {
+        Name: "ProductID",
+        Type: "text",
+        Caption: "Mã sản phẩm",
+        DataSourceMember: "ProductID",
+        Width: 150
+    },
+    {
+        Name: "ProductName",
+        Type: "text",
+        Caption: "Tên sản phẩm",
+        DataSourceMember: "ProductName",
+        Width: 300
+    },
+    {
+        Name: "QuantityUnitName",
+        Type: "text",
+        Caption: "Đơn vị tính",
+        DataSourceMember: "QuantityUnitName",
+        Width: 100
+    },
+    {
+        Name: "Quantity",
+        Type: "textbox",
+        // Type: "textboxNewGroup",
+        Caption: "Số lượng nhập trả",
+        Value: '',
+        labelError: 'số lượng nhập trả',
+        DataSourceMember: "Quantity",
+        Width: 150,
+        // validatonList: [ "number"],
+        IsNoneZero: false
+    },
+   
+];
+
+
+export const GridMLObjectDefinition = [
+    {
+        Name: "MtreturnRequestTypeID",
+        DefaultValue: "",
+        BindControlName: "MtreturnRequestTypeID",
+        DataSourceMember: "MtreturnRequestTypeID"
+    },
+    {
+        Name: "MaterialGroupID",
+        DefaultValue: "",
+        BindControlName: "MaterialGroupID",
+        DataSourceMember: "MaterialGroupID"
+    },
+    {
+        Name: "ProductID",
+        DefaultValue: "",
+        BindControlName: "ProductID",
+        DataSourceMember: "ProductID"
+    },
+    {
+        Name: "ProductName",
+        DefaultValue: "",
+        BindControlName: "ProductName",
+        DataSourceMember: "ProductName"
+    },
+    {
+        Name: "QuantityUnitName",
+        DefaultValue: "",
+        BindControlName: "QuantityUnitName",
+        DataSourceMember: "QuantityUnitName"
+    },
+    {
+        Name: "Quantity",
+        DefaultValue: "",
+        BindControlName: "Quantity",
+        DataSourceMember: "Quantity"
+    }
 ];
