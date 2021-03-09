e6ab24171129d6467fc976bc400a21f54cc88ce2
74260 <74260@Q9RDPMSO10-62>
2021-03-09 13:34:04 +0700

    update code báo cáo tồn vật tư

--- src/views/TMS/ReportShipmentOrder/InventoryMaterials/Search/index.js
+++ src/views/TMS/ReportShipmentOrder/InventoryMaterials/Search/index.js
@@ -393,7 +393,7 @@ class SearchCom extends React.Component {
                 ProductID: objValue[0].value
             }
             this.props.callFetchAPI(APIHostName, "api/AdvanceDebtFlow/LoadAdvanceDebtFlowUsing", objData).then(apiResult => {
-                // console.log('4:', objData, apiResult)
+                 console.log('4:', objData, apiResult)
                 if (!apiResult.IsError) {
                     this.onShowModal('', status);
                 }
@@ -458,7 +458,7 @@ class SearchCom extends React.Component {
                     IsAutoPaging={true}
                     RowsPerPage={20}
                     ref={this.gridref}
-                    //RequirePermission={SHIPMENTORDER_REPORT_VIEW}
+                    RequirePermission={SHIPMENTORDER_REPORT_VIEW}
                     IsExportFile={true}
                     DataExport={this.state.dataMaterialGroupExport}
                     fileName="Danh sách báo cáo tồn vật tư khác"
