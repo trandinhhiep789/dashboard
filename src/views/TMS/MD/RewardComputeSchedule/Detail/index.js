9ee1ab804cfd2677d88e5652aee8b52f3f1622fe
Mr. Sang <thucvuvan.it@gmail.com>
2021-03-22 11:03:24 +0700

    1

--- src/views/TMS/MD/RewardComputeSchedule/Detail/index.js
+++ src/views/TMS/MD/RewardComputeSchedule/Detail/index.js
@@ -162,8 +162,9 @@ class DetailCom extends React.Component {
                                 <div className="row">
                                     <div className="col-md-6">
                                         <div className="form-group">
-                                            <span>Thời gian tính(tính bằng giây): </span>
-                                            <span>{parseFloat((this.state.DataSource.ComputeInterval / 1000) % 60).toFixed(2)}</span>
+                                            <span>Thời gian tính: </span>
+                                            {/* <span>{parseFloat((this.state.DataSource.ComputeInterval / 1000) % 60).toFixed(2)}</span> */}
+                                            <span>{this.state.DataSource.ComputeIntervalString}</span>
                                         </div>
                                     </div>
 
