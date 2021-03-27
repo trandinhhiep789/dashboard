05e229f68ccb6b00dbbff58be4b15ce810ca8007
DESKTOP-Q1KGFCA\phung <phungtheco1@gmail>
2021-03-27 10:38:49 +0700

    style error boundary component

--- src/actions/ErrorBoundary.js
+++ src/actions/ErrorBoundary.js
@@ -18,19 +18,20 @@ export default class ErrorBoundary extends React.Component {
 
     render() {
         if (this.state.hasError) {
-            // You can render any custom fallback UI
-            return <div className="container">
-                <div className="row justify-content-center">
-                    <div className="col-sm-8">
-                        <div className="row">
-                            <div className="col-sm-6 img-404">
-                                <img src="/src/img/error/404.png" />
-                            </div>
-                            <div className="col-sm-6">
-                                <h1 className="text-danger">404</h1>
-                                <h3>Oops Page Not Found</h3>
-                                <p>The page you are looking for does not exist or has been moved.</p>
-                            </div>
+            return <div className="container d-flex align-items-center justify-content-center"
+                style={{
+                    height: "100vh",
+                    overflow: "auto"
+                }}>
+                <div className="d-flex justify-content-center">
+                    <div className="row">
+                        <div className="col-sm-4 img-404">
+                            <img src="/src/img/error/404.png" />
+                        </div>
+                        <div className="col-sm-8">
+                            <h1 className="text-danger">404</h1>
+                            <h3>Oops Page Not Found</h3>
+                            <p>The page you are looking for does not exist or has been moved.</p>
                         </div>
                     </div>
                 </div>
