54c28f64dfde8dd50849229c17ad144042632990
CoPhung <phungtheco1@gmail.com>
2021-05-05 15:20:31 +0700

    pull code

--- src/utils/function.js
+++ src/utils/function.js
@@ -62,14 +62,13 @@ export const dateToLocalISO = (date) => {
 // }
 
 export const numberDecimalWithComma = (nStr) => {
-    // nStr += "";
-    // let x = nStr.split(".");
-    // let x1 = x[0];
-    // let x2 = x.length > 1 ? "." + x[1] : "";
-    // let rgx = /(\d+)(\d{3})/;
-    // while (rgx.test(x1)) {
-    //     x1 = x1.replace(rgx, "$1" + "," + "$2");
-    // }
-    // return x1 + x2;
-    return nStr;
+    nStr += "";
+    let x = nStr.split(".");
+    let x1 = x[0];
+    let x2 = x.length > 1 ? "." + x[1] : "";
+    let rgx = /(\d+)(\d{3})/;
+    while (rgx.test(x1)) {
+        x1 = x1.replace(rgx, "$1" + "," + "$2");
+    }
+    return x1 + x2;
 }
