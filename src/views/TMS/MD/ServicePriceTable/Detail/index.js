bdc28e11f41f14a8a491a831563d3d48ebc2be2c
PhanNV <vanphan2512@gmail.com>
2021-05-11 15:59:29 +0700

    Dịch vụ bảng giá thưởng

--- src/views/TMS/MD/ServicePriceTable/Detail/index.js
+++ src/views/TMS/MD/ServicePriceTable/Detail/index.js
@@ -14,8 +14,13 @@ import {
     APIHostName,
     DetailPagePath,
     LoadAPIPath,
-    TitleFormDetail
-   
+    TitleFormDetail,
+    DataGridColumnSPTDetailItemList,
+    TitleFromSPTDetail,
+    TitleFromSPTArea,
+    DataGridColumnSPTAreatemList,
+    DeleteAPISPTAreaPath
+
 
 } from "../constants";
 import { MessageModal } from "../../../../../common/components/Modal";
@@ -24,12 +29,16 @@ import ServicePriceTableInfo from "./ServicePriceTableInfo";
 import { showModal, hideModal } from '../../../../../actions/modal';
 import { MODAL_TYPE_COMMONTMODALS } from '../../../../../constants/actionTypes';
 import ReactNotification from "react-notifications-component";
+import ServicePriceTableDetail from "../ServicePriceTableDetail";
+import ServicePriceTableArea from "../ServicePriceTableArea";
 
 
 
 class DetailCom extends React.Component {
     constructor(props) {
         super(props);
+        this.handleInputChangeAraeObjItem = this.handleInputChangeAraeObjItem.bind(this)
+        this.handleInputChangeObjItem = this.handleInputChangeObjItem.bind(this)
         this.state = {
             DataSource: {},
             CallAPIMessage: "",
@@ -37,12 +46,15 @@ class DetailCom extends React.Component {
             IsLoadDataComplete: false,
             IsSystem: false,
             ServicePriceTableID: '',
-         
+            ServicePriceTableDetail: [],
+            ServicePriceTableArea: []
+
         }
         this.notificationDOMRef = React.createRef();
     }
 
     componentDidMount() {
+        console.log('apiResult', this,this.props)
         this.props.updatePagePath(DetailPagePath);
         this.callLoadData(this.props.match.params.id);
         this.setState({
@@ -56,7 +68,7 @@ class DetailCom extends React.Component {
             if (apiResult.IsError) {
                 this.setState({
                     IsCallAPIError: !apiResult.IsError,
-             
+
                 });
                 this.showMessage(apiResult.Message);
             }
@@ -64,6 +76,8 @@ class DetailCom extends React.Component {
 
                 this.setState({
                     DataSource: apiResult.ResultObject,
+                    ServicePriceTableArea: apiResult.ResultObject.ServicePriceTable_AreaList,
+                    ServicePriceTableDetail: apiResult.ResultObject.ServicePriceTableDetailList,
                     IsLoadDataComplete: true,
                     IsSystem: apiResult.ResultObject.IsSystem
                 });
@@ -80,7 +94,6 @@ class DetailCom extends React.Component {
             />
         );
     }
-
 
     addNotification(message1, IsError) {
         let cssNotification, iconNotification;
@@ -112,6 +125,104 @@ class DetailCom extends React.Component {
         });
     }
 
+
+    handleInputChangeObjItem(id, apiResult) {
+        console.log('handleInputChangeObjItem', id, apiResult)
+        if (apiResult.IsError) {
+            this.showMessage(apiResult.Message);
+        }
+        else {
+            this.addNotification(apiResult.Message, apiResult.IsError);
+            this.callLoadData(id);
+            this.props.hideModal();
+        }
+
+    }
+
+
+    handleItemInsertSPTDetail() {
+        this.props.showModal(MODAL_TYPE_COMMONTMODALS, {
+            title: 'Thêm chi tiết bảng giá dịch vụ',
+            content: {
+                text: <ServicePriceTableDetail
+                    dataSource={this.state.DataSource}
+                    onInputChangeObj={this.handleInputChangeObjItem}
+
+                />
+            },
+            maxWidth: '1000px'
+        });
+    }
+
+    handleItemEditSPTDetail() {
+
+    }
+
+    handleItemDeleteSPTDetail() {
+
+    }
+
+    handleInputChangeAraeObjItem(id, apiResult) {
+        console.log('handleInputChangeAraeObjItem', id, apiResult)
+        if (apiResult.IsError) {
+            this.showMessage(apiResult.Message);
+        }
+        else {
+            this.props.hideModal();
+            this.addNotification(apiResult.Message, apiResult.IsError);
+            this.callLoadData(id);
+
+        }
+
+    }
+
+    handleItemInsertSPTArea() {
+        this.props.showModal(MODAL_TYPE_COMMONTMODALS, {
+            title: 'Thêm khu vực áp dụng bảng giá dịch vụ',
+            content: {
+                text: <ServicePriceTableArea
+                    dataSource={this.state.DataSource}
+                    onInputChangeObj={this.handleInputChangeAraeObjItem}
+
+                />
+            },
+            maxWidth: '1000px'
+        });
+    }
+
+    handleItemEditSPTArea(index) {
+        console.log("index", index)
+        this.props.showModal(MODAL_TYPE_COMMONTMODALS, {
+            title: 'Thêm khu vực áp dụng bảng giá dịch vụ',
+            content: {
+                text: <ServicePriceTableArea
+                    dataSource={this.state.DataSource}
+                    index={index}
+                    onInputChangeObj={this.handleInputChangeAraeObjItem}
+
+                />
+            },
+            maxWidth: '1000px'
+        });
+    }
+
+    handleItemDeleteSPTArea(index) {
+        const { ServicePriceTableID, DataSource } = this.state;
+        let MLObject = {};
+        MLObject.AreaID = index;
+        MLObject.ServicePriceTableID = ServicePriceTableID;
+
+        this.props.callFetchAPI(APIHostName, DeleteAPISPTAreaPath, MLObject).then((apiResult) => {
+            this.setState({ IsCallAPIError: apiResult.IsError });
+            this.addNotification(apiResult.Message, apiResult.IsError);
+            if (!apiResult.IsError) {
+                this.callLoadData(ServicePriceTableID);
+            }
+        });
+    }
+
+
+
     render() {
         const { IsSystem } = this.state;
         if (this.state.IsLoadDataComplete) {
@@ -126,6 +237,39 @@ class DetailCom extends React.Component {
 
                             <ServicePriceTableInfo
                                 ServicePriceTableInfo={this.state.DataSource}
+                            />
+
+                            <InputGridControl
+                                name="ServicePriceTableDetail"
+                                controltype="InputGridControl"
+                                title={TitleFromSPTDetail}
+                                IDSelectColumnName={"ServicePriceTableDetailID"}
+                                PKColumnName={"ServicePriceTableDetailID"}
+                                listColumn={DataGridColumnSPTDetailItemList}
+                                dataSource={this.state.ServicePriceTableDetail}
+                                onInsertClick={this.handleItemInsertSPTDetail.bind(this)}
+                                onEditClick={this.handleItemEditSPTDetail.bind(this)}
+                                onDeleteClick={this.handleItemDeleteSPTDetail.bind(this)}
+                                ref={this.gridref}
+                                isSystem={IsSystem}
+                                IsExportFile={false}
+                            />
+
+
+                            <InputGridControl
+                                name="ServicePriceTableArea"
+                                controltype="InputGridControl"
+                                title={TitleFromSPTArea}
+                                IDSelectColumnName={"AreaID"}
+                                PKColumnName={"AreaID"}
+                                listColumn={DataGridColumnSPTAreatemList}
+                                dataSource={this.state.ServicePriceTableArea}
+                                onInsertClick={this.handleItemInsertSPTArea.bind(this)}
+                                onEditClick={this.handleItemEditSPTArea.bind(this)}
+                                onDeleteClick={this.handleItemDeleteSPTArea.bind(this)}
+                                ref={this.gridref}
+                                isSystem={IsSystem}
+                                IsExportFile={false}
                             />
 
                         </div>
