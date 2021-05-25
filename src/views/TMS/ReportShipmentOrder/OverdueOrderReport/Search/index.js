1943be53744f03af4f1812920918bee8e6082037
142188-PhungTheCo <phungtheco1@gmail.com>
2021-05-25 16:21:18 +0700

    export file in form search - OverdueOrderReport

--- src/views/TMS/ReportShipmentOrder/OverdueOrderReport/Search/index.js
+++ src/views/TMS/ReportShipmentOrder/OverdueOrderReport/Search/index.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import ReactNotification from "react-notifications-component";
 import { ModalManager } from 'react-dynamic-modal';
+import * as FileSaver from 'file-saver';
+import * as XLSX from 'xlsx';
 
 import { updatePagePath } from "../../../../../actions/pageAction";
 import { callFetchAPI } from "../../../../../actions/fetchAPIAction";
@@ -23,14 +25,18 @@ class Search extends React.Component {
             gridDataSource: [],
             dataExport: [],
             PageNumber: 1,
+            SearchData: []
         }
 
         this.searchref = React.createRef();
+        this.notificationDOMRef = React.createRef();
         this.handleSearchSubmit = this.handleSearchSubmit.bind(this);
         this.showMessage = this.showMessage.bind(this);
+        this.addNotification = this.addNotification.bind(this);
         this.callSearchData = this.callSearchData.bind(this);
         this.handleExportFile = this.handleExportFile.bind(this);
         this.handleExportFileFormSearch = this.handleExportFileFormSearch.bind(this);
+        this.handleExportExcel = this.handleExportExcel.bind(this);
     }
 
     componentDidMount() {
@@ -43,6 +49,36 @@ class Search extends React.Component {
             onCloseModal={this.handleCloseMessage}
         />);
     };
+
+    addNotification(message1, IsError) {
+        let cssNotification, iconNotification;
+        if (!IsError) {
+            cssNotification = "notification-custom-success";
+            iconNotification = "fa fa-check"
+        } else {
+            cssNotification = "notification-danger";
+            iconNotification = "fa fa-exclamation"
+        }
+        this.notificationDOMRef.current.addNotification({
+            container: "bottom-right",
+            content: (
+                <div className={cssNotification}>
+                    <div className="notification-custom-icon">
+                        <i className={iconNotification} />
+                    </div>
+                    <div className="notification-custom-content">
+                        <div className="notification-close">
+                            <span>×</span>
+                        </div>
+                        <h4 className="notification-title">Thông Báo</h4>
+                        <p className="notification-message">{message1}</p>
+                    </div>
+                </div>
+            ),
+            dismiss: { duration: 6000 },
+            dismissable: { click: true }
+        });
+    }
 
     callSearchData(searchData) {
         this.props.callFetchAPI(APIHostName, "api/ShipmentOrder/OverdueOrderReport", searchData).then(apiResult => {
@@ -102,12 +138,79 @@ class Search extends React.Component {
         this.callSearchData(postData);
     };
 
+    handleExportExcel(dataExport, fileName) {
+        const fileType = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
+        const fileExtension = '.xlsx';
+
+        const ws = XLSX.utils.json_to_sheet(dataExport);
+        const wb = { Sheets: { 'data': ws }, SheetNames: ['data'] };
+        const excelBuffer = XLSX.write(wb, { bookType: 'xlsx', type: 'array' });
+        const data = new Blob([excelBuffer], { type: fileType });
+
+        FileSaver.saveAs(data, fileName + fileExtension);
+
+        const result = {
+            IsError: false,
+            Message: "Xuất file thành công!"
+        }
+
+        this.addNotification(result.Message, result.IsError);
+    }
+
     handleExportFile() {
 
     };
 
     handleExportFileFormSearch(FormData, MLObject) {
-        console.log(FormData, MLObject)
+        const postData = [
+
+            {
+                SearchKey: "@FROMDATE",
+                SearchValue: MLObject.FromDate
+            },
+            {
+                SearchKey: "@TODATE",
+                SearchValue: MLObject.ToDate
+            },
+            {
+                SearchKey: "@TYPECOD",
+                SearchValue: MLObject.COD
+            },
+            {
+                SearchKey: "@SHIPMENTORDERSTATUSGROUPID",
+                SearchValue: MLObject.ShipmentOrderStatusGroupID
+            },
+            {
+                SearchKey: "@Keyword",
+                SearchValue: MLObject.Keyword
+            },
+            {
+                SearchKey: "@TYPENAME",
+                SearchValue: MLObject.Typename
+            }
+
+        ];
+
+        this.props.callFetchAPI(APIHostName, "api/ShipmentOrder/OverdueOrderReportExport", postData).then(apiResult => {
+            if (!apiResult.IsError) {
+                const { ResultObject } = apiResult;
+
+                const dataExport = ResultObject.map(item => {
+                    const result = GridColumnList.reduce((acc, val) => {
+                        return {
+                            ...acc,
+                            [val.Caption]: item[val.DataSourceMember]
+                        }
+                    }, {});
+
+                    return result;
+                });
+
+                this.handleExportExcel(dataExport, "Báo cáo chi tiết vận đơn quá hạn");
+            } else {
+                this.addNotification(apiResult.Message, apiResult.IsError);
+            }
+        });
     };
 
     handleonChangePage(pageNum) {
@@ -124,8 +227,6 @@ class Search extends React.Component {
             }
         });
 
-        console.log("aa", listMLObject)
-
         this.callSearchData(listMLObject);
         this.setState({
             PageNumber: pageNum
@@ -141,10 +242,11 @@ class Search extends React.Component {
 
                 <SearchForm
                     FormName="Tìm kiếm chi tiết vận đơn quá hạn"
+                    className="multiple multiple-custom"
+                    classNamebtnSearch="groupAction"
                     listelement={SearchElementList}
                     MLObjectDefinition={SearchMLObjectDefinition}
                     ref={this.searchref}
-                    className="multiple multiple-custom"
                     IsButtonExport={true}
                     onExportSubmit={this.handleExportFileFormSearch}
                     onSubmit={this.handleSearchSubmit}
