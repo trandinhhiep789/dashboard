104d2ba0962c0c837280c92e1f2fbdad3a34edfa
Mr. Sang <thucvuvan.it@gmail.com>
2021-06-30 09:29:02 +0700

    1

---
+++ src/views/TMS/MD/DocumentType/index.js
@@ -0,0 +1,49 @@
+import React from "react";
+import {
+    BrowserRouter as Router,
+    Route,
+    Switch
+} from "react-router-dom";
+import { connect } from "react-redux";
+import { callFetchAPI } from "../../../../actions/fetchAPIAction";
+import Search from "./Search";
+import Add from "./Add";
+import Edit from "./Edit";
+import NotFound from '../../../NotFound'
+class DocumentsTypeCom extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <Switch>
+                <Route exact path="/DocumentsType" component={Search} />
+                <Route exact path="/DocumentsType/Add" component={Add} />
+                <Route exact path="/DocumentsType/Edit/:id" component={Edit} />
+                <Route path="*" component={NotFound} />
+            </Switch>
+        );
+    }
+}
+
+const mapStateToProps = state => {
+    return {
+        AppInfo: state,
+        FetchAPIInfo: state.FetchAPIInfo
+    };
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+        callFetchAPI: (hostname, hostURL, postData) => {
+            return dispatch(callFetchAPI(hostname, hostURL, postData));
+        }
+    };
+};
+
+const DocumentsType = connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(DocumentsTypeCom);
+export default DocumentsType;
