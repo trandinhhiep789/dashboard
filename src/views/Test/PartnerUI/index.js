a5b4b0ce62c717df9f431a84ad8771fbef3f967f
PhanNV <vanphan2512@gmail.com>
2021-07-30 12:42:25 +0700

    fix loi cssadd UI partner

--- src/views/Test/PartnerUI/index.js
+++ src/views/Test/PartnerUI/index.js
@@ -1,5 +1,10 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import {
+    BrowserRouter as Router,
+    Route,
+    Switch
+} from "react-router-dom";
 import { connect } from 'react-redux';
 import { callFetchAPI } from "../../../actions/fetchAPIAction";
 import { updatePagePath } from "../../../actions/pageAction";
@@ -8,127 +13,30 @@ import DataGrid from "../../../common/components/DataGrid";
 import {
     SearchElementList,
     SearchMLObjectDefinition,
+    DataGridColumnList,
+    BackLink,
+    AddLink,
+    PagePath
 } from "./constants";
-import { MODAL_TYPE_VIEW } from "../../../constants/actionTypes";
 import { showModal, hideModal } from '../../../actions/modal';
-import { TreeSelect, DatePicker } from 'antd';
-const { SHOW_PARENT } = TreeSelect;
+import Search from "./Search";
+import Add from "./Add";
+import Edit from "./Edit";
+import Detail from "./Detail";
+import NotFound from '../../NotFound';
 
-const treeData = [
-    {
-        title: 'Node1',
-        value: '0-0',
-        key: '0-0',
-        children: [
-            {
-                title: 'Child Node1',
-                value: '0-0-0',
-                key: '0-0-0',
-            },
-        ],
-    },
-    {
-        title: 'Node2',
-        value: '0-1',
-        key: '0-1',
-        children: [
-            {
-                title: 'Child Node3',
-                value: '0-1-0',
-                key: '0-1-0',
-            },
-            {
-                title: 'Child Node4',
-                value: '0-1-1',
-                key: '0-1-1',
-            },
-            {
-                title: 'Child Node5',
-                value: '0-1-2',
-                key: '0-1-2',
-            },
-        ],
-    },
-];
-
-const options = [
-    { value: 'chocolate', label: 'Chocolate' },
-    { value: 'strawberry', label: 'Strawberry' },
-    { value: 'vanilla', label: 'Vanilla' }
-]
 class PartnerUICom extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            widthPercent: "",
-            gridDataSource: [],
-        };
-        this.searchref = React.createRef();
-    }
-
-    componentDidMount() {
-        this.updateWindowDimensions();
-        window.addEventListener("resize", this.updateWindowDimensions);
-    }
-
-    componentWillUnmount() {
-        window.removeEventListener("resize", this.updateWindowDimensions);
-    }
-
-    updateWindowDimensions = () => {
-        this.setState({
-            widthPercent: (window.innerWidth * 60) / 100
-        })
-    };
-
-
     
-    handleSearchSubmit(formData, MLObject){
-        console.log("Search", formData, MLObject)
-    }
-
-
     render() {
-     
         return (
-            <React.Fragment>
-                <SearchForm
-                    FormName="Tìm kiếm danh sách vận đơn"
-                    listelement={SearchElementList}
-                    MLObjectDefinition={SearchMLObjectDefinition}
-                    onSubmit={this.handleSearchSubmit.bind(this)}
-                    ref={this.searchref}
-                    colGroupAction={6}
-                    IsButtonExport={false}
-                    IsButtonhistory={false}
-                    className=""
-                />
-
-                {/* <DataGrid
-                    listColumn={DataGridColumnList}
-                    dataSource={this.state.gridDataSource}
-                    AddLink={AddLink}
-                    IDSelectColumnName={IDSelectColumnName}
-                    PKColumnName={PKColumnName}
-                    onDeleteClick={this.handleDelete}
-                    IsDelete={true}
-                    IsAutoPaging={true}
-                    RowsPerPage={10}
-                    RequirePermission={SERVICEAGREEMENT_VIEW}
-                    DeletePermission={SERVICEAGREEMENT_DELETE}
-                    ExportPermission={SERVICEAGREEMENT_EXPORT}
-                    IsExportFile={true}
-                    DataExport={this.state.dataExport}
-                    fileName="Danh sách hợp đồng"
-                    onExportFile={this.handleExportFile.bind(this)}
-                    IsImportFile={true}
-                    SchemaData={schema}
-                    onImportFile={this.handleImportFile.bind(this)}
-
-                /> */}
-                
-
-            </React.Fragment>
+            <Switch>
+                <Route exact path="/PartnerUI" component={Search} />
+                <Route exact path="/PartnerUI/Add/" component={Add} />
+                <Route exact path="/PartnerUI/Edit/:id" component={Edit} />
+                <Route exact path="/PartnerUI/Detail/:id" component={Detail} />
+        
+                <Route path="*" component={NotFound} />
+            </Switch>
         );
     }
 
