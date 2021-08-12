4865d6d8f4096434c249be6cf57a219b27620365
PIORS <PIORS@WINDOWS-F5IL54M>
2021-08-12 10:27:10 +0700

    hoclenho

--- src/views/TMS/ShipmentRoute/Component/ListShipCoordinator.js
+++ src/views/TMS/ShipmentRoute/Component/ListShipCoordinator.js
@@ -37,7 +37,9 @@ class ListShipCoordinatorCom extends Component {
             IsCallAPIError: false,
             ShipmentRouteID: this.props.ShipmentRouteID,
             ShipmentRouteLst: [],
-            ShipmentOrderSameLst: this.props.ShipmentOrderSame
+            ShipmentOrderSameLst: this.props.ShipmentOrderSame,
+            Via_Durations: 0,
+            Via_Distances: ""
 
         }
         this.notificationDOMRef = React.createRef();
@@ -47,7 +49,9 @@ class ListShipCoordinatorCom extends Component {
         if (JSON.stringify(this.props.InfoCoordinator) !== JSON.stringify(nextProps.InfoCoordinator)) {
             this.setState({
                 ShipmentOrder: nextProps.InfoCoordinator,
-                ShipmentOrderSameLst: nextProps.ShipmentOrderSame
+                ShipmentOrderSameLst: nextProps.ShipmentOrderSame,
+                Via_Durations: 0,
+                Via_Distances: ""
             })
         }
     }
@@ -424,13 +428,19 @@ class ListShipCoordinatorCom extends Component {
     handleDeleteID = (id) => e => {
         let resultRouteID = this.state.ShipmentOrder.find(n => n.ShipmentOrderID == id).ShipmentRouteID;
         this.state.ShipmentOrder.splice(this.state.ShipmentOrder.findIndex(n => n.ShipmentOrderID == id), 1);
-        if(resultRouteID=="")
-        {
-            this.setState({ ShipmentOrder: this.state.ShipmentOrder });
+        if (resultRouteID == "") {
+            this.setState({
+                ShipmentOrder: this.state.ShipmentOrder,
+                Via_Durations: 0,
+                Via_Distances: ""
+            });
         }
-        else
-        {
-            this.setState({ ShipmentOrder: this.state.ShipmentOrder,ShipmentRouteID:"" });
+        else {
+            this.setState({
+                ShipmentOrder: this.state.ShipmentOrder, ShipmentRouteID: "",
+                Via_Durations: 0,
+                Via_Distances: ""
+            });
         }
     };
     handleChangeOder = (rowIndex, OrderID) => e => {
@@ -477,7 +487,11 @@ class ListShipCoordinatorCom extends Component {
                     if (resultdd == undefined)
                         ShipmentOrder.push(item);
                 });
-                this.setState({ ShipmentRouteID: RouteID, ShipmentOrder: ShipmentOrder });
+                this.setState({
+                    ShipmentRouteID: RouteID, ShipmentOrder: ShipmentOrder,
+                    Via_Durations: 0,
+                    Via_Distances: ""
+                });
             }
             else {
                 this.addNotification(apiResult.Message, apiResult.IsError);
@@ -493,12 +507,70 @@ class ListShipCoordinatorCom extends Component {
             ShipmentOrder.push(resultShipmentOrderSame);
         this.setState({ ShipmentOrder: ShipmentOrder });
     };
-    handleDistances = () => e => {
+    handleDistances = () => {
+        let { ShipmentOrder, ShipmentOrderSameLst } = this.state;
+        let Points = [];
+        ShipmentOrder.map((item, index) => {
+            const Receivervalues = item.ReceiverGeoLocation.split(",");
+            if (Receivervalues == "") {
+                this.addNotification("Không xác định được tạo độ nhà vận đơn " + item.ShipmentOrderID, true);
+            }
+
+            let objReceiverPoints = {
+                "Latitude": Receivervalues[0],
+                "Longitude": Receivervalues[1]
+            };
+            if (index == 0) {
+                const values = ShipmentOrder[0].SenderGeoLocation.split(",");
+                let objPoints = {
+                    "Latitude": values[0],
+                    "Longitude": values[1]
+                };
+                Points.push(objPoints);
+            }
+            Points.push(objReceiverPoints);
+            if (ShipmentOrder.length - 1 == index) {
+                const values = ShipmentOrder[0].SenderGeoLocation.split(",");
+                let objPoints = {
+                    "Latitude": values[0],
+                    "Longitude": values[1]
+                };
+                Points.push(objPoints);
+            }
+        });
+
+        let paramsRequest = {
+            "Alternative": 2147483647,
+            "Distance": true,
+            "Duration": true,
+            "Geometry": true,
+            "Instructions": true,
+            "Points": Points,
+            "RouteCriteria": 0,
+            "Uturn": true,
+            "VehicleType": 2
+        };
+        let resultPoints = Points.find(n => n.Latitude == "");
+        if (resultPoints == undefined) {
+            this.props.callFetchAPI(APIHostName, 'api/Maps/FindPathViaRoute', paramsRequest).then((apiResult) => {
+                if (!apiResult.IsError) {
+                    let Durationslst = JSON.parse(apiResult.ResultObject).Value.Routes[0].Via_Durations;
+                    let Distanceslst = JSON.parse(apiResult.ResultObject).Value.Routes[0].Via_Distances;
+                    // console.log("Durationslst", Math.floor(Durationslst[Durationslst.length-1]/60))
+                    // console.log("Durationslst", Distanceslst[Distanceslst.length-1]/1000)
+                    this.setState({
+                        Via_Durations: Math.floor(Durationslst[Durationslst.length - 1] / 60),
+                        Via_Distances: Distanceslst[Distanceslst.length - 1] >= 1000 ? Distanceslst[Distanceslst.length - 1] / 1000 + "km" : Distanceslst[Distanceslst.length - 1] + "m"
+                    });
+                }
+            });
+
+        }
     };
     render() {
-        let { ShipmentOrder, ShipmentRouteID, ShipmentRouteLst, ShipmentOrderSameLst } = this.state;
+        let { ShipmentOrder, ShipmentRouteID, ShipmentRouteLst, ShipmentOrderSameLst, Via_Distances, Via_Durations } = this.state;
         let resultShipmentRoute = ShipmentRouteLst.filter(n => n.ShipmentRouteID != ShipmentRouteID);
-        console.log("resultShipmentRoute", resultShipmentRoute)
+       // console.log("resultShipmentRoute", resultShipmentRoute)
         return (
             <React.Fragment>
                 <div className="card">
@@ -776,7 +848,7 @@ class ListShipCoordinatorCom extends Component {
                                     resultShipmentRoute.map((item, index) => {
                                         return (
                                             <div className="col-md-6 col-lg-4">
-                                                <div className="card card-secondary">
+                                                <div key={"Route" + index} className="card card-secondary">
                                                     <div className="card-body">
                                                         <ul onClick={this.handleClickRoute(item.ShipmentRouteID)} >
                                                             <li className="item infoOder">
@@ -808,7 +880,7 @@ class ListShipCoordinatorCom extends Component {
                                         // console.log("resultdd",resultdd)
                                         if (resultdd == undefined) {
                                             return (
-                                                <div className="col-md-6 col-lg-4">
+                                                <div key={"Same" + index} className="col-md-6 col-lg-4">
                                                     <div className="card card-secondary">
                                                         <div className="card-body">
                                                             <ul onClick={this.handleClickShipmentOrderSame(item.ShipmentOrderID)} >
@@ -846,6 +918,13 @@ class ListShipCoordinatorCom extends Component {
                     </div>
                 </div>
                 <div className="modal-footer modal-footer-center">
+                    {Via_Distances != "" ? (
+                        <React.Fragment>
+                            <span>Km ước lượng:{Via_Distances}</span>
+                            <span>di chuyển {Via_Durations}'</span>
+                        </React.Fragment>
+
+                    ) : ""}
                     {ShipmentOrder.length > 1 ?
                         (
                             <button className="btn btn-w-md btn-round btn-info ml-10" type="button" onClick={this.handleDistances.bind(this)}>Tính khoản cách</button>
