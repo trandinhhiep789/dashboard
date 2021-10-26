1355157005f5140cb7876ba876dfa45351b9b924
164578 - Hoai Duc <hoaiducit215@gmail.com>
2021-10-26 09:22:08 +0700

    Cập nhật Khai báo Model phương tiện

--- src/views/TMS/MD/VehicleModel/Add/index.js
+++ src/views/TMS/MD/VehicleModel/Add/index.js
@@ -2,7 +2,8 @@ import { APIHostName, AddAPIPath, AddElementList, AddPagePath, BackLink, MLObjec
 import { callClearLocalCache, callGetCache } from "../../../../../actions/cacheAction";
 
 import { ERPCOMMONCACHE_VEHICLETYPE } from "./../../../../../constants/keyCache";
-import FormContainer from "./../../../../../common/components/Form/AdvanceForm/FormContainer/index";
+import FormContainer from "./../../../../../common/components/FormContainer/index";
+import FormControl from "../../../../../common/components/FormContainer/FormControl";
 import { MD_VEHICLEMODEL_ADD } from "./../../../../../constants/functionLists";
 import { MessageModal } from "../../../../../common/components/Modal";
 import { ModalManager } from "react-dynamic-modal";
@@ -33,21 +34,20 @@ class AddCom extends React.Component {
     MLObject.CreatedUser = this.props.AppInfo.LoginInfo.Username;
     MLObject.LoginLogID = JSON.parse(this.props.AppInfo.LoginInfo.TokenString).AuthenLogID;
 
-    console.log(MLObject);
-
+    console.log(formData.cbVehicleTypeID);
     if (MLObject.VehicleTypeID === "-1") {
-      this.showMessage("Loại xe không được bỏ trống");
-      this.setState({ IsCallAPIError: true });
+      formData.cbVehicleTypeID.ErrorLst.IsValidatonError = true;
+      formData.cbVehicleTypeID.ErrorLst.ValidatonErrorMessage = "Loại xe không được bỏ trống";
       return;
     }
 
-    this.props.callFetchAPI(APIHostName, AddAPIPath, MLObject).then((apiResult) => {
-      this.setState({ IsCallAPIError: apiResult.IsError });
-      if (!apiResult.IsError) {
-        this.props.callClearLocalCache(ERPCOMMONCACHE_VEHICLETYPE);
-      }
-      this.showMessage(apiResult.Message);
-    });
+    // this.props.callFetchAPI(APIHostName, AddAPIPath, MLObject).then((apiResult) => {
+    //   this.setState({ IsCallAPIError: apiResult.IsError });
+    //   if (!apiResult.IsError) {
+    //     this.props.callClearLocalCache(ERPCOMMONCACHE_VEHICLETYPE);
+    //   }
+    //   this.showMessage(apiResult.Message);
+    // });
   }
 
   handleCloseMessage() {
@@ -67,18 +67,84 @@ class AddCom extends React.Component {
     }
 
     return (
-      <SimpleForm
-        BackLink={BackLink}
-        dataSource={this.state.DataSource}
-        FormMessage={this.state.CallAPIMessage}
-        FormName={TitleFormAdd}
-        IsErrorMessage={false}
-        listelement={AddElementList}
-        MLObjectDefinition={MLObjectDefinition}
-        onSubmit={this.handleSubmit}
-        ref={this.searchref}
-        RequirePermission={MD_VEHICLEMODEL_ADD}
-      />
+      // <SimpleForm
+      //   BackLink={BackLink}
+      //   dataSource={this.state.DataSource}
+      //   FormMessage={this.state.CallAPIMessage}
+      //   FormName={TitleFormAdd}
+      //   IsErrorMessage={this.state.IsCallAPIError}
+      //   listelement={AddElementList}
+      //   MLObjectDefinition={MLObjectDefinition}
+      //   onSubmit={this.handleSubmit}
+      //   ref={this.searchref}
+      //   RequirePermission={MD_VEHICLEMODEL_ADD}
+      // />
+
+      <FormContainer FormName={TitleFormAdd} MLObjectDefinition={MLObjectDefinition} listelement={[]} onSubmit={this.handleSubmit} BackLink={BackLink} RequirePermission={MD_VEHICLEMODEL_ADD}>
+        <div className="row">
+          <div className="col-md-12">
+            <FormControl.ComboBoxSelect
+              name="cbVehicleTypeID"
+              colspan="10"
+              labelcolspan="2"
+              label="Loại xe"
+              isautoloaditemfromcache={true}
+              loaditemcachekeyid="ERPCOMMONCACHE.VEHICLETYPE"
+              valuemember="VehicleTypeID"
+              nameMember="VehicleTypeName"
+              controltype="InputControl"
+              value="-1"
+              listoption={null}
+              datasourcemember="VehicleTypeID"
+              placeholder="---Vui lòng chọn---"
+              isMultiSelect={false}
+              validatonList={["required"]}
+            />
+          </div>
+          <div className="col-md-12">
+            <FormControl.TextBox
+              name="txtVehicleModelName"
+              colspan="10"
+              labelcolspan="2"
+              readOnly={false}
+              label="Tên model xe"
+              placeholder="Tên model xe"
+              controltype="InputControl"
+              value=""
+              maxSize={9}
+              datasourcemember="VehicleModelName"
+              validatonList={["required"]}
+            />
+          </div>
+          <div className="col-md-12">
+            <FormControl.CheckBox
+              name="chkIsActived"
+              colspan="10"
+              labelcolspan="2"
+              readOnly={false}
+              label="Kích hoạt"
+              controltype="InputControl"
+              value={true}
+              datasourcemember="IsActived"
+              classNameCustom="customCheckbox"
+            />
+          </div>
+
+          <div className="col-md-12">
+            <FormControl.CheckBox
+              name="chkIsSystem"
+              colspan="10"
+              labelcolspan="2"
+              readOnly={false}
+              label="Hệ thống"
+              controltype="InputControl"
+              value=""
+              datasourcemember="IsSystem"
+              classNameCustom="customCheckbox"
+            />
+          </div>
+        </div>
+      </FormContainer>
     );
   }
 }
