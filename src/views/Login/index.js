7b9b261c84d750415ed6d7c3339c7cb1baa289f6
HiepTran <trandinhhiep789@gmail.com>
2022-02-14 19:35:32 +0700

    feat: ui login

--- src/views/Login/index.js
+++ src/views/Login/index.js
@@ -1,168 +1,209 @@
-import React from "react";
-import { connect } from 'react-redux';
-import { ModalManager } from 'react-dynamic-modal';
-import { Button } from 'antd'
+import React from 'react'
+import { connect } from 'react-redux'
+import { Button, Input, notification } from 'antd'
+import { UserOutlined, LockOutlined } from '@ant-design/icons'
 
-import { setCookie, getCookie } from "../../common/library/CommonLib.js";
-import { MessageModal } from "../../common/components/Modal";
-import MD5Digest from "../../common/library/cryptography/MD5Digest.js";
-import { AUTHEN_HOSTNAME, COOKIELOGIN, SESSION_EXPIRE_MINUTE } from "../../constants/systemVars.js";
-import { callRegisterClient } from "../../actions/registerClient";
-import { loginSuccess, callLogin, logout } from "../../actions/loginAction";
-
+import { setCookie, getCookie } from '../../common/library/CommonLib.js'
+import MD5Digest from '../../common/library/cryptography/MD5Digest.js'
+import { AUTHEN_HOSTNAME, COOKIELOGIN, SESSION_EXPIRE_MINUTE } from '../../constants/systemVars.js'
+import { callRegisterClient } from '../../actions/registerClient'
+import { loginSuccess, callLogin, logout } from '../../actions/loginAction'
+import './Login.css'
 
 class LoginCom extends React.Component {
-    constructor(props) {
-        super(props);
-        this.handleLogin = this.handleLogin.bind(this);
-        this.onTextChange = this.onTextChange.bind(this);
-        this.state = {
-            IsClickLoginButton: false,
-            IsLoginSuccess: false,
-            txtUserName: "",
-            txtPassword: "",
-            LoginMessage: "",
-            txtremember: false
-        };
+  constructor(props) {
+    super(props)
+    this.handleLogin = this.handleLogin.bind(this)
+    this.onTextChange = this.onTextChange.bind(this)
+    this.state = {
+      IsClickLoginButton: false,
+      IsLoginSuccess: false,
+      txtUserName: '',
+      txtPassword: '',
+      LoginMessage: '',
+      txtremember: false
     }
+  }
 
-    showMessage(message) {
-        ModalManager.open(<MessageModal title="Thông báo"
-            message={message} onRequestClose={() => true}
-        />);
+  onTextChange(e) {
+    const ischecked = e.target.type == 'checkbox' ? e.target.checked : false
+    const name = e.target.name
+    let value = e.target.value
+    if (e.target.type == 'checkbox') {
+      value = ischecked
     }
+    this.setState({ [name]: value })
+  }
 
-    onTextChange(e) {
-        const ischecked = e.target.type == 'checkbox' ? e.target.checked : false;
-        const name = e.target.name;
-        let value = e.target.value;
-        if (e.target.type == 'checkbox') {
-            value = ischecked;
+  onShowPassChange(e) {
+    const ischecked = e.target.type == 'checkbox' ? e.target.checked : false
+    var x = document.getElementsByName('txtPassword')[0]
+    if (x.type === 'password' && ischecked) {
+      x.type = 'text'
+    } else {
+      x.type = 'password'
+    }
+  }
+
+  registerClient(username, password) {
+    this.props.callRegisterClient(AUTHEN_HOSTNAME, username, password).then(registerResult => {
+      if (!registerResult.IsError) {
+        this.callLogin(username, password)
+      } else {
+        this.setState({ LoginMessage: registerResult.Message })
+      }
+    })
+  }
+
+  callLogin = (username, password) => {
+    this.props.callLogin(username, password).then(loginResult => {
+      if (!loginResult.IsError) {
+        this.setState({ IsLoginSuccess: true })
+        var LoginInfo = JSON.stringify(this.props.AuthenticationInfo.LoginInfo)
+        localStorage.setItem('LoginInfo', LoginInfo)
+        if (this.state.txtremember) {
+          setCookie(COOKIELOGIN, LoginInfo, SESSION_EXPIRE_MINUTE)
         }
-        this.setState({ [name]: value });
+        const { from } = this.props.location.state || { from: { pathname: '/' } }
+        this.props.history.push(from)
+        this.setState({ IsClickLoginButton: true })
+      } else {
+        this.setState({ IsClickLoginButton: false })
+        this.openNotificationWithIcon('error', loginResult.Message)
+      }
+    })
+  }
+
+  componentDidMount() {
+    let sessionlogin = getCookie(COOKIELOGIN)
+    if (sessionlogin) {
+      let LoginInfo = JSON.parse(sessionlogin)
+      //   console.log("componentDidMount login",LoginInfo);
+      this.props.loginSuccess(LoginInfo.LoginUserInfo, LoginInfo.TokenString, LoginInfo.Password)
+      this.setState({ IsLoginSuccess: true })
     }
+  }
 
-    onShowPassChange(e){
-        const ischecked = e.target.type == 'checkbox' ? e.target.checked : false;
-        var x = document.getElementsByName("txtPassword")[0];
-        if (x.type === "password" && ischecked) {
-            x.type = "text";
-        } else {
-            x.type = "password";
-        }
+  handleLogin(e) {
+    e.preventDefault()
+    this.setState({ IsClickLoginButton: true })
+    const userName = this.state.txtUserName
+    const passWord = MD5Digest(this.state.txtPassword)
+    if (userName && this.state.txtPassword) {
+      this.registerClient(userName, passWord)
+    } else {
+      this.openNotificationWithIcon('error', 'Vui lòng nhập tên truy cập hoặc mật khẩu')
+      this.setState({ IsClickLoginButton: false })
     }
+  }
 
-    registerClient(username, password) {
-        this.props.callRegisterClient(AUTHEN_HOSTNAME, username, password).then((registerResult) => {
-            if (!registerResult.IsError) {
-                this.callLogin(username, password);
-            }
-            else {
-                this.setState({ LoginMessage: registerResult.Message });
-            }
-        });
-    }
+  openNotificationWithIcon(type, message) {
+    notification[type]({
+      message: 'Thông báo',
+      description: message
+    })
+  }
 
-    callLogin = (username, password) => {
-        this.props.callLogin(username, password).then((loginResult) => {
-            if (!loginResult.IsError) {
-                this.setState({ IsLoginSuccess: true });
-                var LoginInfo = JSON.stringify(this.props.AuthenticationInfo.LoginInfo);
-                localStorage.setItem('LoginInfo', LoginInfo)
-                if (this.state.txtremember) {
-                    setCookie(COOKIELOGIN, LoginInfo, SESSION_EXPIRE_MINUTE);
-                }
-                const { from } = this.props.location.state || { from: { pathname: '/' } }
-                this.props.history.push(from)
-                this.setState({ IsClickLoginButton: true })
-            }
-            else {
-
-                this.setState({ IsClickLoginButton: false })
-                this.showMessage(loginResult.Message);
-            }
-        });
-    }
-
-    componentDidMount() {
-        let sessionlogin = getCookie(COOKIELOGIN);
-        if (sessionlogin) {
-            let LoginInfo = JSON.parse(sessionlogin);
-            //   console.log("componentDidMount login",LoginInfo);
-            this.props.loginSuccess(LoginInfo.LoginUserInfo, LoginInfo.TokenString, LoginInfo.Password);
-            this.setState({ IsLoginSuccess: true })
-        }
-    }
-
-    handleLogin(e) {
-        e.preventDefault();
-        this.setState({ IsClickLoginButton: true });
-        const userName = this.state.txtUserName;
-        const passWord = MD5Digest(this.state.txtPassword);
-        if (userName && this.state.txtPassword) {
-            this.registerClient(userName, passWord);
-        } else {
-            this.showMessage("Vui lòng nhập tên truy cập hoặc mật khẩu");
-            this.setState({ IsClickLoginButton: false })
-        }
-    }
-
-    render() {
-        return (
-            <div className="container">
-                <div className="cardlogin card-container">
-                    <img id="profile-img" className="profile-img-card" src="src/img/avatar/avatar_2x.png" />
-                    <p id="profile-name" className="profile-name-card"></p>
-                    <form className="form-signin">
-                        <span id="reauth-email" className="reauth-email"></span>
-                        <input type="text" id="inputEmail" className="form-control" placeholder="Tên truy cập" autoFocus name="txtUserName" value={this.state.txtUserName} onChange={this.onTextChange} />
-                        <input type="password" id="inputPassword" className="form-control" placeholder="Mật khẩu" name="txtPassword" value={this.state.txtPassword} onChange={this.onTextChange} />
-                        <div className="form-group flexbox flex-column flex-md-row w-100">
-                            <div className="custom-control custom-checkbox">
-                                <input type="checkbox" name="txtremember" id="txtremember" checked={this.state.txtremember} onChange={this.onTextChange} className="custom-control-input" />
-                                <label className="custom-control-label" htmlFor="txtremember" >Nhớ mật khẩu</label>
-                            </div>
-                            <a className="text-muted hover-primary fs-13 mt-2 mt-md-0" href="#">Quên mật khẩu?</a>
-                        </div>
-
-                        <div className="form-group flexbox flex-column flex-md-row w-100">
-                            <div className="custom-control custom-checkbox">
-                                <input type="checkbox" name="txtshowpass" id="txtshowpass" onChange={this.onShowPassChange} className="custom-control-input" />
-                                <label className="custom-control-label" htmlFor="txtshowpass" >Hiển thị mật khẩu</label>
-                            </div>
-                        </div>
-
-                        {/* <button className="btn btn-lg btn-primary btn-block btn-signin" type="submit" onClick={this.handleLogin} >Đăng nhập</button> */}
-                        <Button className="btn btn-lg btn-primary btn-block btn-signin" onClick={this.handleLogin} loading={this.state.IsClickLoginButton}>Đăng nhập</Button>
-                    </form>
-                </div>
+  render() {
+    return (
+      <div className="LoginCom">
+        <div className="LoginCom__content">
+          <div className="LoginCom__content__img">
+            <img className="LoginCom__content__imgAvata" src="src/img/avatar/avatar_2x.png" />
+          </div>
+          <br />
+          <form className="form-signin">
+            <Input
+              placeholder="Tên truy cập"
+              autoFocus
+              name="txtUserName"
+              value={this.state.txtUserName}
+              onChange={this.onTextChange}
+              prefix={<UserOutlined />}
+              size="large"
+            />
+            <br />
+            <br />
+            <Input
+              type="password"
+              placeholder="Mật khẩu"
+              name="txtPassword"
+              value={this.state.txtPassword}
+              onChange={this.onTextChange}
+              prefix={<LockOutlined />}
+              size="large"
+            />
+            <br />
+            <br />
+            <div>
+              <div>
+                <input
+                  type="checkbox"
+                  name="txtremember"
+                  id="txtremember"
+                  checked={this.state.txtremember}
+                  onChange={this.onTextChange}
+                  className="custom-control-input"
+                />
+                <label className="custom-control-label" htmlFor="txtremember">
+                  Nhớ mật khẩu
+                </label>
+              </div>
             </div>
-        );
-    }
+            <div>
+              <div>
+                <input
+                  type="checkbox"
+                  name="txtshowpass"
+                  id="txtshowpass"
+                  onChange={this.onShowPassChange}
+                  className="custom-control-input"
+                />
+                <label className="custom-control-label" htmlFor="txtshowpass">
+                  Hiển thị mật khẩu
+                </label>
+              </div>
+            </div>
+
+            <br />
+            <Button
+              type="primary"
+              block
+              shape="round"
+              loading={this.state.IsClickLoginButton}
+              onClick={this.handleLogin}
+            >
+              Đăng nhập
+            </Button>
+          </form>
+        </div>
+      </div>
+    )
+  }
 }
 
 const mapStateToProps = state => {
-    return {
-        AuthenticationInfo: state
-    }
+  return {
+    AuthenticationInfo: state
+  }
 }
 
 const mapDispatchToProps = dispatch => {
-    return {
-        callLogin: (username, password) => {
-            return dispatch(callLogin(username, password))
-        },
-        loginSuccess: (loginInfo, token) => {
-            return dispatch(loginSuccess(loginInfo, token))
-        },
-        callRegisterClient: (hostname, username, password) => {
-            return dispatch(callRegisterClient(hostname, username, password));
-        },
-        logout: () => {
-            dispatch(logout())
-        },
+  return {
+    callLogin: (username, password) => {
+      return dispatch(callLogin(username, password))
+    },
+    loginSuccess: (loginInfo, token) => {
+      return dispatch(loginSuccess(loginInfo, token))
+    },
+    callRegisterClient: (hostname, username, password) => {
+      return dispatch(callRegisterClient(hostname, username, password))
+    },
+    logout: () => {
+      dispatch(logout())
     }
+  }
 }
 
-const Login = connect(mapStateToProps, mapDispatchToProps)(LoginCom);
-export default Login;
+const Login = connect(mapStateToProps, mapDispatchToProps)(LoginCom)
+export default Login
