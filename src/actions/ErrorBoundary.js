30daa8b678c9a458fd52db583acfecebaf3615e3
HiepTran <trandinhhiep789@gmail.com>
2022-02-16 13:59:50 +0700

    fix: ui ErrorBoudary.js

--- src/actions/ErrorBoundary.js
+++ src/actions/ErrorBoundary.js
@@ -1,45 +1,45 @@
-import React from 'react';
+import React from 'react'
 
 export default class ErrorBoundary extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { hasError: false };
-    }
+  constructor(props) {
+    super(props)
+    this.state = { hasError: false }
+  }
 
-    static getDerivedStateFromError(error) {
-        // Update state so the next render will show the fallback UI.
-        return { hasError: true };
-    }
+  static getDerivedStateFromError(error) {
+    // Update state so the next render will show the fallback UI.
+    return { hasError: true }
+  }
 
-    componentDidCatch(error, info) {
-        // Log the error to an error reporting service
-        if (error) {
-            console.error("ErrorBoundary", info.componentStack)
-        }
+  componentDidCatch(error, info) {
+    // Log the error to an error reporting service
+    if (error) {
+      console.error('ErrorBoundary', info.componentStack)
     }
+  }
 
-    render() {
-        if (this.state.hasError) {
-            return <div className="container d-flex align-items-center justify-content-center"
-                style={{
-                    height: "100vh",
-                    overflow: "auto"
-                }}>
-                <div className="d-flex justify-content-center">
-                    <div className="row">
-                        <div className="col-sm-4 img-404">
-                            <img src="/src/img/error/404.png" />
-                        </div>
-                        <div className="col-sm-8">
-                            <h1 className="text-danger">404</h1>
-                            <h3>Oops Page Not Found</h3>
-                            <p>The page you are looking for does not exist or has been moved.</p>
-                        </div>
-                    </div>
-                </div>
+  render() {
+    if (this.state.hasError) {
+      return (
+        <div
+          style={{ width: '100%', height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}
+        >
+          <div style={{ display: 'flex' }}>
+            <div className="">
+              <img src="/src/img/error/404.png" />
             </div>
-        }
-
-        return this.props.children;
+            <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+              <div>
+                <h1 style={{ color: 'red' }}>404</h1>
+                <h3>Oops Page Not Found</h3>
+                <p>The page you are looking for does not exist or has been moved.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      )
     }
+
+    return this.props.children
+  }
 }
